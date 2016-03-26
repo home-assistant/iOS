dca652f0f80bfa0811c23e773e3a3151501d4bc7
Robbie Trencheny <me@robbiet.us>
2016-03-25 21:26:21 -0700

    Initial Commit

---
+++ HomeAssistant/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HomeAssistant
+//
+//  Created by Robbie Trencheny on 3/25/16.
+//  Copyright © 2016 Robbie Trencheny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
