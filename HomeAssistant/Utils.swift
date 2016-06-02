11aa184cae3d6cc1cd22d1fe2fd5fc89163f6ca8
Robbie Trencheny <me@robbiet.us>
2016-06-02 12:02:37 -0700

    Add String extensions for subscripting

--- HomeAssistant/Utils.swift
+++ HomeAssistant/Utils.swift
@@ -353,3 +353,20 @@ extension UIImage{
         return newImage
     }
 }
+
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.startIndex.advancedBy(i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = startIndex.advancedBy(r.startIndex)
+        let end = start.advancedBy(r.endIndex - r.startIndex)
+        return self[Range(start ..< end)]
+    }
+}
