5320700efd1fc09bdd0f89f2b30b1b7b480cac7a
Robbie Trencheny <me@robbiet.us>
2016-09-09 23:19:10 -0700

    Update NotificationContentExtension

--- NotificationContentExtension/NotificationViewController.swift
+++ NotificationContentExtension/NotificationViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import UserNotifications
 import UserNotificationsUI
+import MapKit
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
@@ -20,7 +21,53 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     }
     
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = notification.request.content.body
+        switch (notification.request.content.categoryIdentifier) {
+            case "mapNotification":
+                buildMapView(notification)
+            default:
+                buildMapView(notification)
+        }
+    }
+    
+    func buildMapView(_ notification: UNNotification) {
+        let haDict = notification.request.content.userInfo["homeassistant"] as! [String:Any]
+        let latitude = haDict["latitude"] as! CLLocationDegrees
+        let longitude = haDict["longitude"] as! CLLocationDegrees
+        
+        let mapCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        
+        let options = MKMapSnapshotOptions()
+        options.mapType = .standard
+        options.showsPointsOfInterest = false
+        options.showsBuildings = false
+        options.region = MKCoordinateRegion(center: mapCoordinate, span: span)
+        options.size = self.view.frame.size
+        options.scale = self.view.contentScaleFactor
+        
+        let snapshotter = MKMapSnapshotter(options: options)
+        snapshotter.start() { snapshot, error in
+            
+            let image = snapshot!.image
+            
+            let pin = MKPinAnnotationView(annotation: nil, reuseIdentifier: "")
+            let pinImage = pin.image
+            
+            UIGraphicsBeginImageContextWithOptions(image.size, true, image.scale);
+            
+            image.draw(at: CGPoint(x: 0, y: 0))
+            
+            let homePoint = snapshot?.point(for: mapCoordinate)
+            pinImage?.draw(at: homePoint!)
+            
+            let finalImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            let imageView = UIImageView(image: finalImage)
+            imageView.frame = self.view.frame
+            imageView.contentMode = .scaleAspectFit
+            self.view.addSubview(imageView)
+        }
     }
 
 }
