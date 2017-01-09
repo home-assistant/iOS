1b3288fcbf3f80d065598296348c5558fef49969
Robbie Trencheny <me@robbiet.us>
2017-01-08 23:09:39 -0800

    Generate screenshots and update iTC metadata

--- HomeAssistantUITests/HomeAssistantUITests.swift
+++ HomeAssistantUITests/HomeAssistantUITests.swift
@@ -27,46 +27,46 @@ class HomeAssistantUITests: XCTestCase {
     }
 
     func testExample() {
-        print("TEST EXAMPLE")
-        print("RUN CONFIGURATION")
+        
         let app = XCUIApplication()
-        let tabBarsQuery = app.tabBars
+        snapshot("00Launch")
+        let welcomeAlert = app.alerts["Welcome"]
+        let myawesomeemailGmailComTextField = welcomeAlert.collectionViews.textFields["myawesomeemail@gmail.com"]
+        myawesomeemailGmailComTextField.tap()
+        myawesomeemailGmailComTextField.typeText("me@robbiet.us")
+        welcomeAlert.buttons["OK"].tap()
+        
         let tablesQuery2 = app.tables
         let tablesQuery = tablesQuery2
-//        snapshot("0Launch")
-//        app.alerts["Welcome"].collectionViews.textFields["myawesomeemail@gmail.com"].typeText("me@robbiet.us\r")
-//
-//        tablesQuery.textFields["https://homeassistant.myhouse.com"].tap()
-//        tablesQuery2.cells.containing(.staticText, identifier:"URL").children(matching: .textField).element.typeText("privatedemo.home-assistant.io")
-//        let textValue = tablesQuery2.cells.containing(.staticText, identifier:"URL").children(matching: .textField).element.value as! String
-//        XCTAssert(textValue == "https://privatedemo.home-assistant.io")
-//
-//        let connectStaticText = tablesQuery.staticTexts["Connect"]
-//        connectStaticText.tap()
-//        tablesQuery.staticTexts["Password"].tap()
-//        tablesQuery2.cells.containing(.staticText, identifier:"Password").children(matching: .secureTextField).element.typeText("demoprivate")
-//        snapshot("01Connection")
-//        connectStaticText.tap()
-//        app.alerts["Connected"].buttons["OK"].tap()
-        print("INSPECTENTITIES")
-
-        snapshot("2DefaultGroupView")
-
-        tabBarsQuery.buttons["All Switches"].tap()
-
-        tablesQuery.staticTexts["AC"].tap()
-        tablesQuery.switches["AC"].tap()
-        snapshot("3SingleEntity")
-        app.navigationBars["AC"].buttons["All Switches"].tap()
-        snapshot("3SingleEntityOn")
-        app.tabBars.buttons["people"].tap()
-        tabBarsQuery.buttons["All Devices"].tap()
-        app.tables.staticTexts["Anne Therese"].tap()
-        app.navigationBars["45.8601, -119.6936"].buttons["All Devices"].tap()
-        tabBarsQuery.buttons["All Lights"].tap()
+        tablesQuery.textFields["https://homeassistant.myhouse.com"].tap()
+        tablesQuery2.cells.containing(.staticText, identifier:"URL").children(matching: .textField).element.typeText("privatedemo.home-assistant.io")
+        let textValue = tablesQuery2.cells.containing(.staticText, identifier:"URL").children(matching: .textField).element.value as! String
+        XCTAssert(textValue == "https://privatedemo.home-assistant.io")
         
-        tablesQuery.staticTexts["Ceiling Lights"].tap()
-        tablesQuery.switches["Ceiling Lights"].tap()
+        let connectStaticText = tablesQuery.staticTexts["Connect"]
+        connectStaticText.tap()
+        tablesQuery.staticTexts["Password"].tap()
+        tablesQuery2.cells.containing(.staticText, identifier:"Password").children(matching: .secureTextField).element.typeText("demoprivate")
+        connectStaticText.tap()
+        app.navigationBars["Settings"].buttons["Done"].tap()
+
+        snapshot("01GroupView", waitForLoadingIndicator: true)
+
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["All Switches"].tap()
+        
+        tablesQuery.staticTexts["Decorative Lights"].tap()
+        snapshot("02SingleEntity")
+        tablesQuery.switches["Decorative Lights"].tap()
+        app.navigationBars["Decorative Lights"].buttons["All Switches"].tap()
+        tabBarsQuery.buttons["More"].tap()
+        snapshot("03ShowAllTabs")
+        app.navigationBars["More"].buttons["Edit"].tap()
+        app.navigationBars.buttons["Done"].tap()
+        app.staticTexts["automations"].tap()
+        tablesQuery.staticTexts["Notify Anne Therese is home"].tap()
+        snapshot("04ShowAutomationEntity")
+
 
     }
 }
