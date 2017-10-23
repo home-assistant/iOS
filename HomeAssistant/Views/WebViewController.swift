7a9630d9f7466ea828194589caa86832ab42447b
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2017-10-22 17:41:34 -0700

    Remove commented code and stub comments from WebViewController

--- HomeAssistant/Views/WebViewController.swift
+++ HomeAssistant/Views/WebViewController.swift
@@ -79,7 +79,6 @@ class WebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, C
             let navController = UINavigationController(rootViewController: settingsView)
             self.present(navController, animated: true, completion: nil)
         }
-        // Do any additional setup after loading the view.
     }
 
     // Workaround for webview rotation issues: https://github.com/Telerik-Verified-Plugins/WKWebView/pull/263
@@ -90,21 +89,6 @@ class WebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, C
             self.webView?.layoutIfNeeded()
         }, completion:nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     override func viewDidAppear(_ animated: Bool) {
         var toolbarItems: [UIBarButtonItem] = []
