0187e95638cddcfcb41e70a66425c6c831354c81
Robbie Trencheny <me@robbiet.us>
2018-10-01 09:59:58 -0700

    Fix getLocalizableSampleTemplate

--- WatchAppExtension/ComplicationController.swift
+++ WatchAppExtension/ComplicationController.swift
@@ -74,7 +74,8 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     func getLocalizableSampleTemplate(for complication: CLKComplication,
                                       withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+
+        handler(ComplicationGroupMember(family: complication.family).errorTemplate)
     }
 
 }
