9d07b85e1ef4ea7e709516315bb17bcd51d4a43a
Stephan Vanterpool <svanterpool@lyft.com>
2018-11-25 10:10:35 -0800

    Send location on region exit

--- Shared/API/Models/DeviceTrackerSee.swift
+++ Shared/API/Models/DeviceTrackerSee.swift
@@ -52,6 +52,9 @@ public class DeviceTrackerSee: Mappable {
             self.SetLocation(location: location)
         } else if let zone = zone {
             self.SetZone(zone: zone)
+            if let location = location, (trigger == .BeaconRegionExit || trigger == .GPSRegionExit) {
+               self.SetLocation(location: location)
+            }
         }
     }
 
