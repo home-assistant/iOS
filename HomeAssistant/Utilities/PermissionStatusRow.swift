7f74d9c5b84516d6f9f67acfc6a7da2824316583
Zac West <zacwest@gmail.com>
2020-06-14 13:01:16 -0700

    Settings Permissions: Add Background Refresh
    This adds a Background Refresh status row and adds disabling like:
    - Zone enter/exit: when location isn't always
    - Background fetch: when location isn't always or background refresh is off
    - Sig loc change: when location isn't always
    - Push notification: when location isn't always

---
+++ HomeAssistant/Utilities/PermissionStatusRow.swift
@@ -0,0 +1,104 @@
+import Foundation
+import Eureka
+import UIKit
+import CoreLocation
+import CoreMotion
+
+public final class LocationPermissionRow: Row<LabelCellOf<CLAuthorizationStatus>>, RowType {
+    required public init(tag: String?) {
+        super.init(tag: tag)
+
+        displayValueFor = { value in
+            guard let value = value else { return nil }
+
+            switch value {
+            case .authorizedAlways:
+                return L10n.SettingsDetails.Location.LocationPermission.always
+            case .authorizedWhenInUse:
+                return L10n.SettingsDetails.Location.LocationPermission.whileInUse
+            case .denied, .restricted:
+                return L10n.SettingsDetails.Location.LocationPermission.never
+            case .notDetermined:
+                return L10n.SettingsDetails.Location.LocationPermission.needsRequest
+            @unknown default:
+                return L10n.SettingsDetails.Location.LocationPermission.never
+            }
+        }
+    }
+}
+
+@available(iOS 11, *)
+public final class MotionPermissionRow: Row<LabelCellOf<CMAuthorizationStatus>>, RowType {
+    required public init(tag: String?) {
+        super.init(tag: tag)
+
+        displayValueFor = { value in
+            guard let value = value else { return nil }
+
+            switch value {
+            case .authorized:
+                return L10n.SettingsDetails.Location.MotionPermission.enabled
+            case .denied, .restricted:
+                return L10n.SettingsDetails.Location.MotionPermission.denied
+            case .notDetermined:
+                return L10n.SettingsDetails.Location.MotionPermission.needsRequest
+            @unknown default:
+                return L10n.SettingsDetails.Location.MotionPermission.denied
+            }
+        }
+    }
+}
+
+public final class BackgroundRefreshStatusRow: Row<LabelCellOf<UIBackgroundRefreshStatus>>, RowType {
+    required public init(tag: String?) {
+        super.init(tag: tag)
+
+        displayValueFor = { value in
+            guard let value = value else { return nil }
+
+            switch value {
+            case .restricted, .denied:
+                return L10n.SettingsDetails.Location.BackgroundRefresh.disabled
+            case .available:
+                return L10n.SettingsDetails.Location.BackgroundRefresh.enabled
+            @unknown default:
+                return L10n.SettingsDetails.Location.BackgroundRefresh.disabled
+            }
+        }
+    }
+}
+
+extension Condition {
+    static var locationPermissionNotAlways: Condition {
+        .function(["locationPermission"], { form in
+            guard let row = form.rowBy(tag: "locationPermission") as? LocationPermissionRow else {
+                return true
+            }
+
+            switch row.value {
+            case .some(.authorizedAlways):
+                return false
+            default:
+                return true
+            }
+        })
+    }
+
+    static var locationNotAlwaysOrBackgroundRefreshNotAvailable: Condition {
+        return .function(["locationPermission", "backgroundRefresh"], { form in
+            guard
+                let locationPermissionRow = form.rowBy(tag: "locationPermission") as? LocationPermissionRow,
+                let backgroundRefreshRow = form.rowBy(tag: "backgroundRefresh") as? BackgroundRefreshStatusRow
+            else {
+                return true
+            }
+
+            switch (locationPermissionRow.value, backgroundRefreshRow.value) {
+            case (.some(.authorizedAlways), .some(.available)):
+                return false
+            default:
+                return true
+            }
+        })
+    }
+}
