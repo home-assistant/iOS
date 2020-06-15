47c8eae6229160feef2c49b2275ac2429d3145f7
Zac West <74188+zacwest@users.noreply.github.com>
2020-06-15 14:18:06 -0700

    Append User-Agent rather than resetting it (#632)
    Refs #555.

--- HomeAssistant/Views/WebViewController.swift
+++ HomeAssistant/Views/WebViewController.swift
@@ -151,9 +151,9 @@ class WebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, U
         }
 
         config.userContentController = userContentController
+        config.applicationNameForUserAgent = SessionManager.defaultHTTPHeaders["User-Agent"]
 
         self.webView = WKWebView(frame: self.view!.frame, configuration: config)
-        self.webView.customUserAgent = SessionManager.defaultHTTPHeaders["User-Agent"]
         self.webView.isOpaque = false
         self.view!.addSubview(webView)
 
