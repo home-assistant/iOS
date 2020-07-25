4d09d636198a94f4f2297f0b3a2604a6aa28bf3e
Zac West <74188+zacwest@users.noreply.github.com>
2020-07-25 15:25:24 -0700

    "Add to Shortcuts" button in Action configurator (#834)
    - Restores the cancel button for Action editing.
    - Adds the Add to Shortcuts button in the "Name" section.

---
+++ HomeAssistant/Views/VoiceShortcutRow.swift
@@ -0,0 +1,101 @@
+import Foundation
+import Eureka
+import IntentsUI
+import Shared
+
+@available(iOS 13.5, *)
+public class VoiceShortcutCell: Cell<INShortcut>, CellType,
+    INUIAddVoiceShortcutButtonDelegate,
+    INUIAddVoiceShortcutViewControllerDelegate,
+    INUIEditVoiceShortcutViewControllerDelegate {
+    private let button = INUIAddVoiceShortcutButton(style: .automatic)
+
+    fileprivate var shortcutRow: VoiceShortcutRow? { return row as? VoiceShortcutRow }
+
+    public override func setup() {
+        super.setup()
+        selectionStyle = .none
+
+        contentView.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        let margins = contentView.layoutMarginsGuide
+
+        isAccessibilityElement = false // container
+        textLabel?.isHidden = true
+
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: margins.topAnchor),
+            button.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+            button.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            button.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        ])
+
+        button.delegate = self
+    }
+
+    public override func update() {
+        super.update()
+        button.setStyle(shortcutRow?.buttonStyle ?? .automatic)
+        button.shortcut = row.value
+    }
+
+    public func present(
+        _ addVoiceShortcutViewController: INUIAddVoiceShortcutViewController,
+        for addVoiceShortcutButton: INUIAddVoiceShortcutButton
+    ) {
+        addVoiceShortcutViewController.delegate = self
+        formViewController()?.present(addVoiceShortcutViewController, animated: true, completion: nil)
+    }
+
+    public func present(
+        _ editVoiceShortcutViewController: INUIEditVoiceShortcutViewController,
+        for addVoiceShortcutButton: INUIAddVoiceShortcutButton
+    ) {
+        editVoiceShortcutViewController.delegate = self
+        formViewController()?.present(editVoiceShortcutViewController, animated: true, completion: nil)
+    }
+
+    public func addVoiceShortcutViewController(
+        _ controller: INUIAddVoiceShortcutViewController,
+        didFinishWith voiceShortcut: INVoiceShortcut?,
+        error: Error?
+    ) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    public func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    public func editVoiceShortcutViewController(
+        _ controller: INUIEditVoiceShortcutViewController,
+        didUpdate voiceShortcut: INVoiceShortcut?,
+        error: Error?
+    ) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    public func editVoiceShortcutViewController(
+        _ controller: INUIEditVoiceShortcutViewController,
+        didDeleteVoiceShortcutWithIdentifier deletedVoiceShortcutIdentifier: UUID
+    ) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    public func editVoiceShortcutViewControllerDidCancel(
+        _ controller: INUIEditVoiceShortcutViewController
+    ) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+@available(iOS 13.5, *)
+public final class VoiceShortcutRow: Row<VoiceShortcutCell>, RowType {
+    var buttonStyle: INUIAddVoiceShortcutButtonStyle = .automatic
+
+    public required init(tag: String?) {
+        super.init(tag: tag)
+        displayValueFor = { _ in nil }
+    }
+}
