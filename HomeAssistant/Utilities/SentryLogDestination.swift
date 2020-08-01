2ba2432ccccff67d81b3e9c3880a0b713766b9b3
Zac West <74188+zacwest@users.noreply.github.com>
2020-07-31 18:28:45 -0700

    Add Sentry & Remove Firebase{Crashlytics, Analytics} (#854)
    Required making more generic the messaging around the privacy toggles. It is worth noting that the previous Crash Reporting preference is lost since it was using Firebase directly to store it.
    
    Features like automatic/logging-based breadcrumbs, error logging and messaging are controlled by the analytics key. The crash reporting feature is controlled by crash reporting.

---
+++ HomeAssistant/Utilities/SentryLogDestination.swift
@@ -0,0 +1,35 @@
+import Foundation
+import XCGLogger
+import Sentry
+import Shared
+
+open class SentryLogDestination: BaseQueuedDestination {
+    open override func output(logDetails: LogDetails, message: String) {
+        guard Current.settingsStore.privacy.analytics else {
+            return
+        }
+
+        let breadcrumb = Breadcrumb(level: .init(xcgLogLevel: logDetails.level), category: "log")
+        breadcrumb.message = message
+        SentrySDK.addBreadcrumb(crumb: breadcrumb)
+    }
+}
+
+private extension SentryLevel {
+    init(xcgLogLevel: XCGLogger.Level) {
+        self = {
+            switch xcgLogLevel {
+            case .verbose: return .debug
+            case .debug: return .debug
+            case .info: return .info
+            case .notice: return .warning
+            case .warning: return .warning
+            case .error: return .error
+            case .severe: return .fatal
+            case .alert: return .fatal
+            case .emergency: return .fatal
+            case .none: return .none
+            }
+        }()
+    }
+}
--- Shared/Resources/Swiftgen/Strings.swift
+++ Shared/Resources/Swiftgen/Strings.swift
@@ -1742,16 +1742,18 @@ internal enum L10n {
       /// Privacy
       internal static let title = L10n.tr("Localizable", "settings_details.privacy.title")
       internal enum Analytics {
-        /// Allows collection of basic information about your device and interactions with the app. No user identifiable data is shared with Google, including your Home Assistant URLs and tokens. You must restart the app for changes to this setting to take effect.
-        internal static let description = L10n.tr("Localizable", "settings_details.privacy.analytics.description")
-        /// Google Analytics
-        internal static let title = L10n.tr("Localizable", "settings_details.privacy.analytics.title")
+        /// Allows collection of basic information about your device and interactions with the app. No user identifiable data is shared, including your Home Assistant URLs and tokens. You must restart the app for changes to this setting to take effect.
+        internal static let genericDescription = L10n.tr("Localizable", "settings_details.privacy.analytics.generic_description")
+        /// Analytics
+        internal static let genericTitle = L10n.tr("Localizable", "settings_details.privacy.analytics.generic_title")
       }
-      internal enum Crashlytics {
-        /// Crashlytics allows for deeper tracking of crashes and other errors in the app, leading to faster fixes being published. No user identifiable information is sent, other than basic device information. You must restart the app for changes to this setting to take effect.
-        internal static let description = L10n.tr("Localizable", "settings_details.privacy.crashlytics.description")
-        /// Firebase Crashlytics
-        internal static let title = L10n.tr("Localizable", "settings_details.privacy.crashlytics.title")
+      internal enum CrashReporting {
+        /// Allows for deeper tracking of crashes and other errors in the app, leading to faster fixes being published. No user identifiable information is sent, other than basic device information. You must restart the app for changes to this setting to take effect.
+        internal static let description = L10n.tr("Localizable", "settings_details.privacy.crash_reporting.description")
+        /// This feature currently uses Sentry as the report destination.
+        internal static let sentry = L10n.tr("Localizable", "settings_details.privacy.crash_reporting.sentry")
+        /// Crash Reporting
+        internal static let title = L10n.tr("Localizable", "settings_details.privacy.crash_reporting.title")
       }
       internal enum Messaging {
         /// Firebase Cloud Messaging must be enabled for push notifications to function.
