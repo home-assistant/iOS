9bce84118f620e1920d695cba5c3d06e917d1e9f
Zac West <zacwest@gmail.com>
2020-08-10 20:13:02 -0700

    Basic Perform Action widget that doesn't do anything on activation

--- Widgets/PerformActionWidget.swift
+++ Widgets/PerformActionWidget.swift
@@ -1,52 +1,64 @@
 import WidgetKit
 import SwiftUI
+import Shared
+import Intents
 
-struct PerformActionWidgetProvider: TimelineProvider {
-    // start beta 3 compatibility code
+struct PerformActionWidgetProvider: IntentTimelineProvider {
+    typealias Intent = PerformActionIntent
+
+    // start beta 3 compatibility code for github actions
+    // swiftlint:disable line_length
     typealias Entry = PerformActionEntry
 
-    func snapshot(with context: Self.Context, completion: @escaping (Self.Entry) -> Void) {
-        getSnapshot(in: context, completion: completion)
+    func snapshot(for configuration: PerformActionIntent, with context: Context, completion: @escaping (Self.Entry) -> Void) {
+        getSnapshot(for: configuration, in: context, completion: completion)
     }
 
-    func timeline(with context: Self.Context, completion: @escaping (Timeline<Self.Entry>) -> Void) {
-        getTimeline(in: context, completion: completion)
+    func timeline(for configuration: PerformActionIntent, with context: Context, completion: @escaping (Timeline<Self.Entry>) -> Void) {
+        getTimeline(for: configuration, in: context, completion: completion)
     }
+    // swiftlint:enable line_length
     // end beta 3 compatibility code
 
-    func getSnapshot(in context: Context, completion: @escaping (PerformActionEntry) -> Void) {
-        completion(.init(text: "snapshot"))
+    func placeholder(in context: Self.Context) -> Self.Entry {
+        PerformActionEntry(configuration: PerformActionIntent())
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<PerformActionEntry>) -> Void) {
-        completion(.init(entries: [PerformActionEntry(text: "timeline")], policy: .atEnd))
+    func getSnapshot(for configuration: Intent, in context: Context, completion: @escaping (Entry) -> Void) {
+        completion(.init(configuration: configuration))
     }
 
-    func placeholder(in context: Context) -> PerformActionEntry {
-        .init(text: "placeholder")
+    func getTimeline(for configuration: Intent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        completion(.init(entries: [
+            .init(configuration: configuration)
+        ], policy: .never))
     }
 }
 
 struct PerformActionEntry: TimelineEntry {
     var date: Date = Date()
-    var text: String
+    var configuration: PerformActionIntent
 }
 
 struct PerformActionView: View {
     var entry: PerformActionEntry
 
     var body: some View {
-        Text(entry.text)
+        Text(entry.configuration.action?.displayString ?? "no action")
     }
 }
 
 struct PerformActionWidget: Widget {
+    static let kind = "PerformAction"
+
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: "Test", provider: PerformActionWidgetProvider()) { (entry: PerformActionEntry) in
-            PerformActionView(entry: entry)
-        }
-        .configurationDisplayName("Widget")
-        .description("Widgets")
+        IntentConfiguration(
+            kind: Self.kind,
+            intent: PerformActionIntent.self,
+            provider: PerformActionWidgetProvider(),
+            content: { PerformActionView(entry: $0) }
+        )
+        .configurationDisplayName("Perform Action")
         .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
     }
 }
