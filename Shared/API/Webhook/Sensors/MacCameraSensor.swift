be7beb76a1c9cd635a550c18ca75259b7f461827
Zac West <zacwest@gmail.com>
2020-08-28 15:05:14 -0700

    Actually add object to list of observed ones

--- Shared/API/Webhook/Sensors/MacCameraSensor.swift
+++ Shared/API/Webhook/Sensors/MacCameraSensor.swift
@@ -22,6 +22,7 @@ private class LiveUpdateInfo: SensorProviderLiveUpdateInfo {
             Current.Log.info("camera info updated for \(id)")
             self?.handler()
         })
+        observedObjects.insert(object)
     }
 }
 
