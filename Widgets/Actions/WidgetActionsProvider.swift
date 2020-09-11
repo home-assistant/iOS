14e71be78f78903d95e0e98a82639803fafc2c82
Zac West <zacwest@gmail.com>
2020-09-10 21:06:44 -0700

    Provide default Actions to unconfigured widget

--- Widgets/Actions/WidgetActionsProvider.swift
+++ Widgets/Actions/WidgetActionsProvider.swift
@@ -8,49 +8,52 @@ struct WidgetActionsEntry: TimelineEntry {
 
 struct WidgetActionsProvider: IntentTimelineProvider {
     typealias Intent = WidgetActionsIntent
-
-    // start beta 3 compatibility code for github actions
     typealias Entry = WidgetActionsEntry
 
-    func snapshot(for configuration: Intent, with context: Context, completion: @escaping (Entry) -> Void) {
-        getSnapshot(for: configuration, in: context, completion: completion)
-    }
-
-    func timeline(for configuration: Intent, with context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        getTimeline(for: configuration, in: context, completion: completion)
-    }
-    // swiftlint:enable line_length
-    // end beta 3 compatibility code
-
-    func placeholder(in context: Context) -> WidgetActionsEntry {
-        .init()
-    }
-
-    func getSnapshot(for configuration: Intent, in context: Context, completion: @escaping (Entry) -> Void) {
-        if context.isPreview {
-            let count: Int = {
-                switch context.family {
-                case .systemSmall: return 1
-                case .systemMedium: return 4
-                case .systemLarge: return 8
-                @unknown default: return 8
-                }
-            }()
-
-            let actions = stride(from: 0, to: count, by: 1).map { _ in
-                with(Action()) {
-                    $0.Text = "Redacted Text"
-                }
-            }
-            completion(.init(actions: actions))
-        } else {
-            completion(.init(actions: configuration.actions?.compactMap { $0.asAction() } ?? []))
+    private static func actionCount(for family: WidgetFamily) -> Int {
+        switch family {
+        case .systemSmall: return 1
+        case .systemMedium: return 4
+        case .systemLarge: return 8
+        @unknown default: return 8
         }
     }
 
+    func placeholder(in context: Context) -> WidgetActionsEntry {
+        let actions = stride(from: 0, to: Self.actionCount(for: context.family), by: 1).map { _ in
+            with(Action()) {
+                $0.Text = "Redacted Text"
+                $0.IconName = MaterialDesignIcons.bedEmptyIcon.name
+            }
+        }
+
+        return .init(actions: actions)
+    }
+
+    private static func defaultActions(in context: Context) -> [Action] {
+        let allActions = Current.realm().objects(Action.self).sorted(byKeyPath: #keyPath(Action.Position))
+        let maxCount = Self.actionCount(for: context.family)
+
+        switch allActions.count {
+        case 0: return []
+        case ...maxCount: return Array(allActions)
+        default: return Array(allActions[0 ..< maxCount])
+        }
+    }
+
+    private static func entry(for configuration: Intent, in context: Context) -> Entry {
+        if let existing = configuration.actions?.compactMap({ $0.asAction() }), !existing.isEmpty {
+            return .init(actions:  existing)
+        } else {
+            return .init(actions: Self.defaultActions(in: context))
+        }
+    }
+
+    func getSnapshot(for configuration: Intent, in context: Context, completion: @escaping (Entry) -> Void) {
+        completion(Self.entry(for: configuration, in: context))
+    }
+
     func getTimeline(for configuration: Intent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        completion(.init(entries: [
-            .init(actions: configuration.actions?.compactMap { $0.asAction() } ?? [])
-        ], policy: .never))
+        completion(.init(entries: [ Self.entry(for: configuration, in: context) ], policy: .never))
     }
 }
