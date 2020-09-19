48365fe82a850645ac4162fbdba4008885f8e9b4
Zac West <zacwest@gmail.com>
2020-09-19 11:31:57 -0700

    Add "Configure…" to Actions menu

--- HomeAssistant/Scenes/SettingsSceneDelegate.swift
+++ HomeAssistant/Scenes/SettingsSceneDelegate.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import Shared
 
 @available(iOS 13, *)
 @objc class SettingsSceneDelegate: BasicSceneDelegate {
@@ -7,6 +8,19 @@ import UIKit
         .init(
             title: L10n.Settings.NavigationBar.title,
             rootViewController: UINavigationController(rootViewController: SettingsViewController())
+        )
+    }
+
+    func pushDetail(group: String, animated: Bool) {
+        guard let navigationController = window?.rootViewController as? UINavigationController else {
+            return
+        }
+
+        navigationController.pushViewController(
+            with(SettingsDetailViewController()) {
+                $0.detailGroup = group
+            },
+            animated: animated
         )
     }
 }
