82622ce9cad7802d6cbccb45c28d92d0b5f38e81
Zac West <zacwest@gmail.com>
2020-10-01 18:28:32 -0700

    Add "Update Sensors" intent
    This is functionally nearly identical to the "Send Location" intent, but doesn't require providing a location to the shortcut to execute, nor does it need to do a network request to geocode, so it should be a lot faster at providing updates.
    
    This is useful for e.g. "When Low Power Mode is turned on or off" or "When (phone) is connected or disconnected from power" automations in Shortcuts.app.
    
    I submitted Intents.strings to Lokalise externally so it should come down in the next download into English as well.

---
+++ Intents/UpdateSensors.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Shared
+import PromiseKit
+
+class UpdateSensorsIntentHandler: NSObject, UpdateSensorsIntentHandling {
+    func handle(intent: UpdateSensorsIntent, completion: @escaping (UpdateSensorsIntentResponse) -> Void) {
+        Current.Log.info("starting")
+
+        firstly {
+            HomeAssistantAPI.authenticatedAPIPromise
+        }.then {
+            $0.UpdateSensors(trigger: .Siri)
+        }.done {
+            Current.Log.info("finished successfully")
+            completion(.init(code: .success, userActivity: nil))
+        }.catch { error in
+            Current.Log.error("failed: \(error)")
+            completion(.init(code: .failure, userActivity: nil))
+        }
+    }
+}
