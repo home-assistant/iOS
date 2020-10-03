4d9a5306372afa9fd105ae82f3e2fe036d88015f
Zac West <74188+zacwest@users.noreply.github.com>
2020-10-03 00:15:04 -0700

    Reorganize files in repo, pull out build settings from pbxproj (#1140)
    This is somewhat in prep of being able to make the project file generated, but also just organizes things into more concrete directory structures.
    
    This pulls out _all_ of the build settings from the root level, and most from the target level, into xcconfigs.
    
    The new directory structure looks like:
    
    - Sources
      - App
        - (everything from HomeAssistant/)
      - WatchApp
      - Shared
      - MacBridge
      - Extensions
        - Intents
        - NotificationContent
        - NotificationService
        - Share
        - Today
        - Watch
        - Widgets
    - Tests
      - App
      - UI
      - Shared
    
    Somewhat intentionally, the file structure under these is not yet standardized/organized.
    
    The project targets are now:
    
    - App
    - WatchApp
    - Shared-iOS
    - Shared-watchOS
    - MacBridge
    - Tests-App
    - Tests-UI
    - Tests-Shared
    - Extension-Intents
    - Extension-NotificationContent
    - Extension-NotificationService
    - Extension-Share
    - Extension-Today
    - Extension-Widget
    - WatchExtension-Watch
    
    This does not yet clean up resources vs. sources, nor does it handle some of the "it's in Sources/App but it's part of Shared" crossover directory issues.

--- icons/Alternates/AlternateGenerator.sh
+++ icons/Alternates/AlternateGenerator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for filename in *.png; do
-  sips -Z 120x120 "$filename" --out "../../HomeAssistant/Resources/Alternate Icons/${filename%.png}@2x.png"
-  sips -Z 180x180 "$filename" --out "../../HomeAssistant/Resources/Alternate Icons/${filename%.png}@3x.png"
+  sips -Z 120x120 "$filename" --out "../../Sources/App/Resources/Alternate Icons/${filename%.png}@2x.png"
+  sips -Z 180x180 "$filename" --out "../../Sources/App/Resources/Alternate Icons/${filename%.png}@3x.png"
 
   echo "<key>${filename%.png}</key>
     <dict>
