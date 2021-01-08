d06edc7cb773d30a675ad4e8fcc269783cdbb98e
Zac West <74188+zacwest@users.noreply.github.com>
2021-01-07 17:26:24 -0800

    Add sensor for frontmost app on Mac (#1352)
    Fixes #977.
    
    ## Summary
    Adds a sensor for the current frontmost app, also known as the current foreground or active app. 
    
    ## Link to pull request in Documentation repository
    Documentation: home-assistant/companion.home-assistant#425
    
    ## Any other notes
    Creates `sensor.frontmost_app` which has a state of the name of the current frontmost app, or None if there isn't one. I'm really not sure how that can happen, but it's definitely possible. This updates via a signal when it changes, so it happens immediately.

---
+++ Sources/Shared/API/Webhook/Sensors/FrontmostAppSensor.swift
@@ -0,0 +1,68 @@
+import Foundation
+import PromiseKit
+
+final class FrontmostAppSensorUpdateSignaler: SensorProviderUpdateSignaler {
+    let signal: () -> Void
+    init(signal: @escaping () -> Void) {
+        self.signal = signal
+
+        #if targetEnvironment(macCatalyst)
+        Current.macBridge.workspaceNotificationCenter.addObserver(
+            self,
+            selector: #selector(frontmostAppDidChange(_:)),
+            name: Current.macBridge.frontmostApplicationDidChangeNotification,
+            object: nil
+        )
+        #endif
+    }
+
+    @objc private func frontmostAppDidChange(_ note: Notification) {
+        signal()
+    }
+}
+
+final class FrontmostAppSensor: SensorProvider {
+    public enum FrostmostAppError: Error, Equatable {
+        case unsupportedPlatform
+    }
+
+    let request: SensorProviderRequest
+    init(request: SensorProviderRequest) {
+        self.request = request
+    }
+
+    func sensors() -> Promise<[WebhookSensor]> {
+        #if targetEnvironment(macCatalyst)
+        var sensors = [WebhookSensor]()
+
+        let frontmost = Current.macBridge.frontmostApplication
+
+        sensors.append(with(WebhookSensor(
+            name: "Frontmost App",
+            uniqueID: "frontmost_app",
+            icon: "mdi:traffic-light",
+            state: frontmost?.localizedName ?? "None"
+        )) {
+            var attributes = [String: Any]()
+
+            attributes["Bundle Identifier"] = frontmost?.bundleIdentifier ?? "N/A"
+            attributes["Launch Date"] = frontmost?.launchDate.map {
+                ISO8601DateFormatter.string(from: $0, timeZone: .current, formatOptions: [
+                    .withInternetDateTime
+                ])
+            } ?? "N/A"
+            attributes["Is Hidden"] = frontmost?.isHidden ?? "N/A"
+            attributes["Owns Menu Bar"] = frontmost?.ownsMenuBar ?? "N/A"
+
+            $0.Attributes = attributes
+        })
+
+        // Set up our observer
+        let _: FrontmostAppSensorUpdateSignaler = request.dependencies.updateSignaler(for: self)
+
+        return .value(sensors)
+        #else
+        return .init(error: FrostmostAppError.unsupportedPlatform)
+        #endif
+    }
+}
