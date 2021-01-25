home-assistant/iOS

commit 5b75b2ea4c526c89a2b82743daed51f09334681b

Move API access into Environment (#1390)
Consolidates REST API access into one entry point, ish.

Sources/Shared/Environment/Environment.swift
@@ -61,7 +61,27 @@ public class Environment {
     public var realmFatalPresentation: ((UIViewController) -> Void)?
     #endif
 
-    public var api: () -> HomeAssistantAPI? = { HomeAssistantAPI.authenticatedAPI() }
+    private var underlyingAPI: Promise<HomeAssistantAPI>?
+
+    public var api: Promise<HomeAssistantAPI> {
+        get {
+            if let value = underlyingAPI {
+                return value
+            } else if !isRunningTests, let value = HomeAssistantAPI() {
+                underlyingAPI = .value(value)
+                return .value(value)
+            } else {
+                return .init(error: HomeAssistantAPI.APIError.notConfigured)
+            }
+        }
+        set {
+            underlyingAPI = newValue
+        }
+    }
+    public func resetAPI() {
+        underlyingAPI = nil
+    }
+
     public var modelManager = ModelManager()
     public var tokenManager: TokenManager?
 
@@ -128,10 +148,6 @@ public class Environment {
     public var isPerformingSingleShotLocationQuery = false
 
     public var backgroundTask: HomeAssistantBackgroundTaskRunner = ProcessInfoBackgroundTaskRunner()
-
-    public func updateWith(authenticatedAPI: HomeAssistantAPI) {
-        self.tokenManager = authenticatedAPI.tokenManager
-    }
 
     // Use of 'appConfiguration' is preferred, but sometimes Beta builds are done as releases.
     public var isTestFlight = Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"

Sources/App/AppDelegate.swift
@@ -66,6 +66,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
+        guard !Current.isRunningTests else {
+            return true
+        }
+
         setDefaults()
 
         Current.backgroundTask = ApplicationBackgroundTaskRunner()
@@ -268,29 +272,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        guard let api = HomeAssistantAPI.authenticatedAPI() else {
-            Current.Log.warning("Background fetch failed because api was not authenticated")
-            completionHandler(.failed)
-            return
-        }
-
         let timestamp = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .full)
         Current.Log.verbose("Background fetch activated at \(timestamp)!")
 
         Current.backgroundTask(withName: "background-fetch") { remaining in
-            let updatePromise: Promise<Void>
+            Current.api.then { api -> Promise<Void> in
+                let updatePromise: Promise<Void>
 
-            if Current.settingsStore.isLocationEnabled(for: UIApplication.shared.applicationState),
-                prefs.bool(forKey: "locationUpdateOnBackgroundFetch") {
-                updatePromise = api.GetAndSendLocation(
-                    trigger: .BackgroundFetch,
-                    maximumBackgroundTime: remaining
-                ).asVoid()
-            } else {
-                updatePromise = api.UpdateSensors(trigger: .BackgroundFetch).asVoid()
+                if Current.settingsStore.isLocationEnabled(for: UIApplication.shared.applicationState),
+                    prefs.bool(forKey: "locationUpdateOnBackgroundFetch") {
+                    updatePromise = api.GetAndSendLocation(
+                        trigger: .BackgroundFetch,
+                        maximumBackgroundTime: remaining
+                    ).asVoid()
+                } else {
+                    updatePromise = api.UpdateSensors(trigger: .BackgroundFetch).asVoid()
+                }
+
+                return updatePromise
             }
-
-            return updatePromise
         }.done {
             completionHandler(.newData)
         }.catch { error in
@@ -448,7 +448,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     return
                 }
 
-                HomeAssistantAPI.authenticatedAPIPromise.then { api in
+                Current.api.then { api in
                     api.HandleAction(actionID: actionID, source: .Watch)
                 }.done { _ in
                     message.reply(.init(identifier: responseIdentifier, content: ["fired": true]))
@@ -512,11 +512,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         let tokenInfo = TokenInfo(accessToken: token, refreshToken: "", expiration: Date.distantFuture)
 
-        let api = HomeAssistantAPI(tokenInfo: tokenInfo)
-
         Current.settingsStore.tokenInfo = tokenInfo
         Current.settingsStore.connectionInfo = connectionInfo
-        Current.updateWith(authenticatedAPI: api)
+        Current.resetAPI()
 
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
             Current.Log.verbose("Requested notifications \(granted), \(String(describing: error))")

Sources/App/LifecycleManager.swift
@@ -40,10 +40,14 @@ class LifecycleManager {
     }
 
     func didFinishLaunching() {
-        _ = HomeAssistantAPI.authenticatedAPI()?.CreateEvent(
-            eventType: "ios.finished_launching",
-            eventData: HomeAssistantAPI.sharedEventDeviceInfo
-        )
+        Current.backgroundTask(withName: "lifecycle-manager-didFinishLaunching") { _ in
+            Current.api.then { api in
+                api.CreateEvent(
+                    eventType: "ios.finished_launching",
+                    eventData: HomeAssistantAPI.sharedEventDeviceInfo
+                )
+            }
+        }.cauterize()
 
         #if !targetEnvironment(macCatalyst)
         Lokalise.shared.checkForUpdates { (updated, error) in
@@ -59,10 +63,15 @@ class LifecycleManager {
     }
 
     @objc private func didEnterBackground() {
-        _ = HomeAssistantAPI.authenticatedAPI()?.CreateEvent(
-            eventType: "ios.entered_background",
-            eventData: HomeAssistantAPI.sharedEventDeviceInfo
-        )
+        Current.backgroundTask(withName: "lifecycle-manager-didEnterBackground") { _ in
+            Current.api.then { api in
+                api.CreateEvent(
+                    eventType: "ios.entered_background",
+                    eventData: HomeAssistantAPI.sharedEventDeviceInfo
+                )
+            }
+        }.cauterize()
+
         invalidatePeriodicUpdateTimer()
     }
 
@@ -82,10 +91,14 @@ class LifecycleManager {
     }
 
     @objc private func didBecomeActive() {
-        _ = HomeAssistantAPI.authenticatedAPI()?.CreateEvent(
-            eventType: "ios.became_active",
-            eventData: HomeAssistantAPI.sharedEventDeviceInfo
-        )
+        Current.backgroundTask(withName: "lifecycle-manager-didBecomeActive") { _ in
+            Current.api.then { api in
+                api.CreateEvent(
+                    eventType: "ios.became_active",
+                    eventData: HomeAssistantAPI.sharedEventDeviceInfo
+                )
+            }
+        }.cauterize()
     }
 
     private func invalidatePeriodicUpdateTimer() {
@@ -116,10 +129,8 @@ class LifecycleManager {
     }
 
     private func connectAPI(reason: HomeAssistantAPI.ConnectReason) {
-        firstly {
-            HomeAssistantAPI.authenticatedAPIPromise
-        }.then { api in
-            return Current.backgroundTask(withName: "connect-api") { _ in
+        return Current.backgroundTask(withName: "connect-api") { _ in
+            Current.api.then { api in
                 api.Connect(reason: reason)
             }
         }.done {

Sources/App/Notifications/NotificationManager.swift
@@ -45,12 +45,6 @@ class NotificationManager: NSObject {
 
         Messaging.messaging().appDidReceiveMessage(userInfo)
 
-        guard let api = HomeAssistantAPI.authenticatedAPI() else {
-            Current.Log.warning("Remote notification handler failed because api was not authenticated")
-            completionHandler(.failed)
-            return
-        }
-
         if let userInfoDict = userInfo as? [String: Any],
             let hadict = userInfoDict["homeassistant"] as? [String: String], let command = hadict["command"] {
                 switch command {
@@ -63,7 +57,9 @@ class NotificationManager: NSObject {
                     Current.Log.verbose("Received remote request to provide a location update")
 
                     Current.backgroundTask(withName: "push-location-request") { remaining in
-                        api.GetAndSendLocation(trigger: .PushNotification, maximumBackgroundTime: remaining)
+                        Current.api.then { api in
+                            api.GetAndSendLocation(trigger: .PushNotification, maximumBackgroundTime: remaining)
+                        }
                     }.done { success in
                         Current.Log.verbose("Did successfully send location when requested via APNS? \(success)")
                         completionHandler(.newData)
@@ -110,12 +106,10 @@ class NotificationManager: NSObject {
 
                 Current.Log.verbose("Success, sending data \(eventData)")
 
-                _ = firstly {
-                    HomeAssistantAPI.authenticatedAPIPromise
-                    }.then { api in
-                        api.CreateEvent(eventType: eventName, eventData: eventData)
-                    }.catch { error -> Void in
-                        Current.Log.error("Received error from createEvent during shortcut run \(error)")
+                Current.api.then { api in
+                    api.CreateEvent(eventType: eventName, eventData: eventData)
+                }.catch { error -> Void in
+                    Current.Log.error("Received error from createEvent during shortcut run \(error)")
                 }
             }
         }
@@ -124,9 +118,7 @@ class NotificationManager: NSObject {
             eventData["status"] = "failure"
             eventData["error"] = error.XCUErrorParameters
 
-            _ = firstly {
-                HomeAssistantAPI.authenticatedAPIPromise
-            }.then { api in
+            Current.api.then { api in
                 api.CreateEvent(eventType: eventName, eventData: eventData)
             }.catch { error -> Void in
                 Current.Log.error("Received error from createEvent during shortcut run \(error)")
@@ -136,9 +128,7 @@ class NotificationManager: NSObject {
         let cancelHandler: CallbackURLKit.CancelCallback = {
             eventData["status"] = "cancelled"
 
-            _ = firstly {
-                HomeAssistantAPI.authenticatedAPIPromise
-            }.then { api in
+            Current.api.then { api in
                 api.CreateEvent(eventType: eventName, eventData: eventData)
             }.catch { error -> Void in
                 Current.Log.error("Received error from createEvent during shortcut run \(error)")
@@ -155,9 +145,7 @@ class NotificationManager: NSObject {
             eventData["status"] = "error"
             eventData["error"] = error.localizedDescription
 
-            _ = firstly {
-                HomeAssistantAPI.authenticatedAPIPromise
-            }.then { api in
+            Current.api.then { api in
                 api.CreateEvent(eventType: eventName, eventData: eventData)
             }.catch { error -> Void in
                 Current.Log.error("Received error from CallbackURLKit perform \(error)")
@@ -229,10 +217,8 @@ extension NotificationManager: UNUserNotificationCenterDelegate {
             )
         }
 
-        firstly {
-            HomeAssistantAPI.authenticatedAPIPromise
-        }.then { api in
-            Current.backgroundTask(withName: "handle-push-action") { _ in
+        Current.backgroundTask(withName: "handle-push-action") { _ in
+            Current.api.then { api in
                 api.handlePushAction(
                     identifier: response.actionIdentifier,
                     category: response.notification.request.content.categoryIdentifier,
@@ -310,11 +296,10 @@ extension NotificationManager: MessagingDelegate {
         Current.crashReporter.setUserProperty(value: fcmToken, name: "FCM Token")
         Current.settingsStore.pushID = fcmToken
 
-        guard let api = HomeAssistantAPI.authenticatedAPI() else {
-            Current.Log.warning("Could not get authenticated API")
-            return
-        }
-
-        _ = api.UpdateRegistration()
+        Current.backgroundTask(withName: "notificationManager-didReceiveRegistrationToken") { _ in
+            Current.api.then { api in
+                api.UpdateRegistration()
+            }
+        }.cauterize()
     }
 }

Sources/App/Onboarding/ConnectInstanceViewController.swift
@@ -135,14 +135,11 @@ class ConnectInstanceViewController: UIViewController {
     }
 
     public func Connect() -> Promise<Void> {
-        guard let api = HomeAssistantAPI.authenticatedAPI(urlConfig: .default, forceInit: true) else {
-            Current.Log.error("Couldn't get authenticated API!")
-            return Promise(error: ConnectionError.noAuthenticatedAPI)
-        }
+        Current.resetAPI()
 
-        return firstly {
-            api.Register()
-        }.then { regResponse -> Promise<MobileAppRegistrationResponse> in
+        return Current.api.then { api in
+            api.Register().map { (api, $0) }
+        }.map { api, regResponse -> HomeAssistantAPI in
             self.setAnimationStatus(self.integrationCreated, state: .success)
 
             let cloudAvailable = (regResponse.CloudhookURL != nil || regResponse.RemoteUIURL != nil)
@@ -156,8 +153,8 @@ class ConnectInstanceViewController: UIViewController {
             let encryptState: AnimationState = regResponse.WebhookSecret != nil ? .success : .failed
             self.setAnimationStatus(self.encrypted, state: encryptState)
 
-            return Promise.value(regResponse)
-        }.then { _ in
+            return api
+        }.then { api in
             return when(fulfilled: [
                 api.GetConfig().asVoid(),
                 Current.modelManager.fetch(),

Sources/App/Settings/ActionConfigurator.swift
@@ -358,17 +358,15 @@ class ActionPreview: UIView {
 
         self.imageView.showActivityIndicator()
 
-        firstly {
-            HomeAssistantAPI.authenticatedAPIPromise
-            }.then { api in
-                api.HandleAction(actionID: action.ID, source: .Preview)
-            }.done { _ in
-                feedbackGenerator.notificationOccurred(.success)
-            }.ensure {
-                self.imageView.hideActivityIndicator()
-            }.catch { err -> Void in
-                Current.Log.error("Error during action event fire: \(err)")
-                feedbackGenerator.notificationOccurred(.error)
+        Current.api.then { api in
+            api.HandleAction(actionID: action.ID, source: .Preview)
+        }.done { _ in
+            feedbackGenerator.notificationOccurred(.success)
+        }.ensure {
+            self.imageView.hideActivityIndicator()
+        }.catch { err -> Void in
+            Current.Log.error("Error during action event fire: \(err)")
+            feedbackGenerator.notificationOccurred(.error)
         }
     }
 }

Sources/App/Settings/AppleWatch/ComplicationEditViewController.swift
@@ -69,7 +69,9 @@ class ComplicationEditViewController: FormViewController, TypedRowControllerType
             Current.Log.error(error)
         }
 
-        HomeAssistantAPI.authenticatedAPI()?.updateComplications(passively: false).cauterize()
+        Current.api.then { api in
+            api.updateComplications(passively: false)
+        }.cauterize()
 
         onDismissCallback?(self)
     }
@@ -97,7 +99,9 @@ class ComplicationEditViewController: FormViewController, TypedRowControllerType
                 Current.Log.error(error)
             }
 
-            HomeAssistantAPI.authenticatedAPI()?.updateComplications(passively: false).cauterize()
+            Current.api.then { api in
+                api.updateComplications(passively: false)
+            }.cauterize()
 
             self.onDismissCallback?(self)
         }))
@@ -484,9 +488,7 @@ class ComplicationEditViewController: FormViewController, TypedRowControllerType
     }
 
     func renderTemplateValue(_ value: String, _ row: BaseRow, expectingPercentile: Bool) {
-        firstly {
-            HomeAssistantAPI.authenticatedAPIPromise
-        }.then {
+        Current.api.then {
             $0.RenderTemplate(templateStr: value)
         }.get { result in
             if expectingPercentile {

Sources/App/Settings/Eureka/AccountRow.swift
@@ -108,9 +108,7 @@ final class HomeAssistantAccountRow: Row<AccountCell>, RowType {
             return
         }
 
-        firstly {
-            HomeAssistantAPI.authenticatedAPIPromise
-        }.then {
+        Current.api.then {
             $0.GetStates()
         }.firstValue {
             $0.Attributes["user_id"] as? String == user.ID

commit 0116f10ab699bd707c150b5a7590255545619c93

Update PromiseKit default map queue, fix crash performing action (#1418)
## Summary
Fixes a crash caused by #1390. Since all API was moved to use promises, and promises' default behavior for `then`/`map` is to dispatch to the main queue, this causes some `Current.api.then…` calls to become unnecessarily synchronous.

## Any other notes
I am not super sure why `.main` as the default for the map queue makes sense. Maybe it prevents some cases where a promise will over-use the thread it's called on. This may also produce some bugs where we previously accessed things on the main queue under the assumption that it defaulted to this behavior.

Also adds a logger to the default PromiseKit prints, so that it routes into normal logs.

Sources/Shared/Environment/Environment.swift
@@ -39,6 +39,22 @@ public var Current = Environment()
 /// unit tests.
 public class Environment {
     internal init() {
+        PromiseKit.conf.Q = (map: nil, return: .main)
+        PromiseKit.conf.logHandler = { event in
+            Current.Log.info {
+                switch event {
+                case .waitOnMainThread:
+                    return "PromiseKit: warning: `wait()` called on main thread!"
+                case .pendingPromiseDeallocated:
+                    return "PromiseKit: warning: pending promise deallocated"
+                case .pendingGuaranteeDeallocated:
+                    return "PromiseKit: warning: pending guarantee deallocated"
+                case .cauterized (let error):
+                    return "PromiseKit:cauterized-error: \(error)"
+                }
+            }
+        }
+
         let crashReporter = CrashReporterImpl()
         self.crashReporter = crashReporter
         crashReporter.setup(environment: self)