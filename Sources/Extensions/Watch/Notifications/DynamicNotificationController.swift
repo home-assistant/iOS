4bbad0ba04e66ecd51e09b76153470b14d09c482
Zac West <74188+zacwest@users.noreply.github.com>
2021-05-03 22:35:18 -0700

    Fix attachments in Watch notifications with dynamic actions (#1619)
    ## Summary
    Implements a similar pipeline for notification content in the watch as the notification content extension.
    
    ## Screenshots
    https://user-images.githubusercontent.com/74188/116958883-d670a600-ac50-11eb-9578-994ee594a271.mp4
    
    (the video starts playing a bit faster when not recording/debugging)
    
    ![Simulator Screen Shot - Apple Watch Series 6 - 44mm - 2021-05-03 at 20 28 19](https://user-images.githubusercontent.com/74188/116958826-ab865200-ac50-11eb-98d4-70394cdd8344.png)
    ![Simulator Screen Shot - Apple Watch Series 6 - 44mm - 2021-05-03 20 22 51](https://user-images.githubusercontent.com/74188/116958827-ac1ee880-ac50-11eb-85ea-58751a0c42aa.png)
    ![Simulator Screen Shot - Apple Watch Series 6 - 44mm - 2021-05-03 20 22 04](https://user-images.githubusercontent.com/74188/116958828-ac1ee880-ac50-11eb-808c-5ea1f947e3a8.png)
    
    ## Any other notes
    - Fixes text input notification actions failing to do anything. Unless we implement the 'suggestions' method (even to just return `[]`) it will fail to show the action, logging something about the `nil` default value being unacceptable.
    - Fixes attachments downloaded by the service extension not showing up at all. By downloading them again, because Apple fails to correctly give us access to them.
    - Adds support for lazily downloading attachments, either when not attached or with the lazy flag.
    - Combines the notification controller (similar to the content extension) for all the various category types.

---
+++ Sources/Extensions/Watch/Notifications/DynamicNotificationController.swift
@@ -0,0 +1,172 @@
+import EMTLoadingIndicator
+import Foundation
+import PromiseKit
+import Shared
+import UserNotifications
+import WatchKit
+
+class DynamicNotificationController: WKUserNotificationInterfaceController {
+    @IBOutlet var loadingImage: WKInterfaceImage!
+    @IBOutlet var errorLabel: WKInterfaceLabel!
+    @IBOutlet var notificationTitleLabel: WKInterfaceLabel!
+    @IBOutlet var notificationSubtitleLabel: WKInterfaceLabel!
+    @IBOutlet var notificationAlertLabel: WKInterfaceLabel!
+    @IBOutlet var notificationImage: WKInterfaceImage!
+    @IBOutlet var notificationVideo: WKInterfaceInlineMovie!
+    @IBOutlet var notificationMap: WKInterfaceMap!
+
+    private var loadingIndicator: EMTLoadingIndicator?
+    private var activeSubController: NotificationSubController? {
+        willSet {
+            if activeSubController !== newValue {
+                activeSubController?.stop()
+            }
+        }
+        didSet {
+            if isActive {
+                start()
+            }
+        }
+    }
+
+    private lazy var dynamicElements: NotificationElements = {
+        .init(image: notificationImage, map: notificationMap, movie: notificationVideo)
+    }()
+
+    private var isActive: Bool = false {
+        didSet {
+            if isActive {
+                start()
+            } else if let subController = activeSubController {
+                subController.stop()
+            }
+        }
+    }
+
+    private var isLoading: Bool = false {
+        didSet {
+            if isLoading {
+                if loadingIndicator == nil {
+                    loadingIndicator = with(EMTLoadingIndicator(
+                        interfaceController: self,
+                        interfaceImage: loadingImage,
+                        width: 40,
+                        height: 40,
+                        style: .dot
+                    )) {
+                        $0.showWait()
+                    }
+                }
+            } else {
+                loadingIndicator?.hide()
+                loadingImage.setHidden(true)
+                loadingIndicator = nil
+            }
+        }
+    }
+
+    override func willActivate() {
+        super.willActivate()
+        isActive = true
+    }
+
+    override func didDeactivate() {
+        super.didDeactivate()
+        isActive = false
+    }
+
+    private func start() {
+        guard let subController = activeSubController else { return }
+
+        isLoading = true
+
+        firstly {
+            subController.start(with: dynamicElements)
+        }.ensure { [self] in
+            isLoading = false
+        }.catch { [self] error in
+            show(error: error)
+        }
+    }
+
+    private func show(error: Error) {
+        errorLabel.setTextColor(.red)
+        errorLabel.setTextAndHideIfEmpty(L10n.NotificationService.failedToLoad + "\n" + error.localizedDescription)
+    }
+
+    private var possibleSubControllers: [NotificationSubController.Type] { [
+        NotificationSubControllerMJPEG.self,
+        NotificationSubControllerMap.self,
+        NotificationSubControllerMedia.self,
+    ] }
+
+    private func subController(for notification: UNNotification) -> NotificationSubController? {
+        for potential in possibleSubControllers {
+            if let controller = potential.init(notification: notification) {
+                return controller
+            }
+        }
+
+        return nil
+    }
+
+    private func subController(for url: URL) -> NotificationSubController? {
+        for potential in possibleSubControllers {
+            if let controller = potential.init(url: url) {
+                return controller
+            }
+        }
+
+        return nil
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        super.didReceive(notification)
+
+        notificationTitleLabel.setTextAndHideIfEmpty(notification.request.content.title)
+        notificationSubtitleLabel.setTextAndHideIfEmpty(notification.request.content.subtitle)
+        notificationAlertLabel.setTextAndHideIfEmpty(notification.request.content.body)
+
+        errorLabel.setHidden(true)
+        dynamicElements.hide()
+
+        notificationActions = notification.request.content.userInfoActions
+
+        if let active = subController(for: notification) {
+            activeSubController = active
+        } else {
+            isLoading = true
+
+            Current.api.then(on: nil) { api in
+                NotificationAttachmentManager().downloadAttachment(from: notification.request.content, api: api)
+            }.tap { [self] result in
+                if case .rejected = result {
+                    // we don't stop loading on success 'cause it's just gonna turn it on again
+                    isLoading = false
+                }
+            }.map { [self] url in
+                subController(for: url)
+            }.done { [self] controller in
+                activeSubController = controller
+            }.catch { [self] error in
+                Current.Log.info("no attachments downloaded: \(error)")
+
+                if error as? NotificationAttachmentManager.ServiceError == .noAttachment {
+                    // can make the above != but it doesn't clearly indicate
+                } else {
+                    show(error: error)
+                }
+            }
+        }
+    }
+
+    override func suggestionsForResponseToAction(
+        withIdentifier identifier: String,
+        for notification: UNNotification,
+        inputLanguage: String
+    ) -> [String] {
+        // if not implemented, this returns `nil` by default, which causes it to not prompt
+        // last tested: watchOS 7.5
+        []
+    }
+}
---
+++ Sources/Extensions/Watch/Notifications/NotificationSubControllerMap.swift
@@ -0,0 +1,93 @@
+import Foundation
+import PromiseKit
+import Shared
+import UserNotifications
+import WatchKit
+
+class NotificationSubControllerMap: NotificationSubController {
+    let location: CLLocationCoordinate2D
+    let secondLocation: CLLocationCoordinate2D?
+
+    required init?(notification: UNNotification) {
+        let userInfo = notification.request.content.userInfo
+
+        guard let haDict = userInfo["homeassistant"] as? [String: Any],
+              let latitude = CLLocationDegrees(templateValue: haDict["latitude"]),
+              let longitude = CLLocationDegrees(templateValue: haDict["longitude"]) else {
+            return nil
+        }
+
+        self.location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+        if let secondLatitude = CLLocationDegrees(templateValue: haDict["second_latitude"]),
+           let secondLongitude = CLLocationDegrees(templateValue: haDict["second_longitude"]) {
+            self.secondLocation = CLLocationCoordinate2D(latitude: secondLatitude, longitude: secondLongitude)
+        } else {
+            self.secondLocation = nil
+        }
+    }
+
+    required init?(url: URL) {
+        nil
+    }
+
+    func start(with elements: NotificationElements) -> Promise<Void> {
+        elements.map.setHidden(false)
+
+        var pinLocations: [CLLocationCoordinate2D] = [location]
+
+        elements.map.addAnnotation(location, with: .red)
+
+        if let secondLocation = secondLocation {
+            pinLocations.append(secondLocation)
+            elements.map.addAnnotation(secondLocation, with: .green)
+        }
+
+        var region = MKCoordinateRegion(
+            center: pinLocations[0],
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        )
+
+        if pinLocations.count > 1 {
+            region = MKCoordinateRegion(coordinates: pinLocations)
+        }
+
+        elements.map.setRegion(region)
+
+        return .value(())
+    }
+
+    func stop() {
+        // nothing to stop
+    }
+}
+
+extension MKCoordinateRegion {
+    init(coordinates: [CLLocationCoordinate2D]) {
+        var minLat: CLLocationDegrees = 90.0
+        var maxLat: CLLocationDegrees = -90.0
+        var minLon: CLLocationDegrees = 180.0
+        var maxLon: CLLocationDegrees = -180.0
+
+        for coordinate in coordinates {
+            let lat = Double(coordinate.latitude)
+            let long = Double(coordinate.longitude)
+            if lat < minLat {
+                minLat = lat
+            }
+            if long < minLon {
+                minLon = long
+            }
+            if lat > maxLat {
+                maxLat = lat
+            }
+            if long > maxLon {
+                maxLon = long
+            }
+        }
+
+        let span = MKCoordinateSpan(latitudeDelta: (maxLat - minLat) * 2.0, longitudeDelta: (maxLon - minLon) * 2.0)
+        let center = CLLocationCoordinate2DMake(maxLat - span.latitudeDelta / 4, maxLon - span.longitudeDelta / 4)
+        self.init(center: center, span: span)
+    }
+}
