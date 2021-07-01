4d4646f81d0d1e50330e6177c3f2374d7c9265ce
Zac West <74188+zacwest@users.noreply.github.com>
2021-07-01 00:40:26 -0700

    Fix tests and Watch App execution in Xcode 12.5 (#1696)
    ## Summary
    Works around Xcode 12.5 / 13.0 (beta 1/2 at least) regressions in target launching.
    
    ## Any other notes
    When running tests for iOS simulator, we get an error like:
    
    > The bundle identifier for Home Assistant Launcher.app couldn’t be read. No such file or directory: “/Users/zac/Library/Developer/Xcode/DerivedData/HomeAssistant-ahjyjjgpphztaebycwnsuwmrdiho/Build/Products/Debug/Home Assistant Launcher.app”.
    
    The Launcher.app target is set to macOS only, so it's not built for iOS simulator. Building it first then executing tests works, so clearly Xcode is incorrectly trying to analyze the target when it isn't used.
    
    This also happens when building for Catalyst, where it can't find the watchOS app which isn't being compiled since it is iOS only:
    
    > The bundle identifier for HomeAssistant-WatchApp.app couldn’t be read. No such file or directory: “/Users/zac/Library/Developer/Xcode/DerivedData/HomeAssistant-ahjyjjgpphztaebycwnsuwmrdiho/Build/Products/Debug-watchos/HomeAssistant-WatchApp.app”.
    
    Filed this as FB9189357 as well.

---
+++ Configuration/Scripts/fix_xcode_12.5_target_platform_issues.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# when building for iOS, Xcode expects the Launcher.app to exist
+LAUNCHER_INFO_PLIST_PATH=$BUILD_DIR/Debug/Home\ Assistant\ Launcher.app/Contents/Info.plist
+if [ ! -f "$LAUNCHER_INFO_PLIST_PATH" ]; then
+	mkdir -p "$(dirname "$LAUNCHER_INFO_PLIST_PATH")"
+
+	cat >"$LAUNCHER_INFO_PLIST_PATH" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleIdentifier</key>
+	<string>io.robbie.HomeAssistant.dev.Launcher</string>
+</dict>
+</plist>
+EOF
+fi
+
+WATCHAPP_INFO_PLIST_PATH=$BUILD_DIR/Debug-watchos/HomeAssistant-WatchApp.app/Contents/Info.plist
+if [ ! -f "$WATCHAPP_INFO_PLIST_PATH" ]; then
+	mkdir -p "$(dirname "$WATCHAPP_INFO_PLIST_PATH")"
+
+	cat >"$WATCHAPP_INFO_PLIST_PATH" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleIdentifier</key>
+	<string>io.robbie.HomeAssistant.dev.watchkitapp</string>
+</dict>
+</plist>
+EOF
+fi
