a7a18e875e1c2780b64933325460e9b70608d8f0
Zac West <zacwest@gmail.com>
2021-10-14 00:37:00 -0700

    Fix not calling WebSocket connect initially

--- Sources/App/Onboarding/API/OnboardingAuthentication.swift
+++ Sources/App/Onboarding/API/OnboardingAuthentication.swift
@@ -179,6 +179,7 @@ class OnboardingAuthentication: NSObject {
                 object: nil,
                 userInfo: nil
             )
+            Current.apiConnection.connect()
         }
     }
 
