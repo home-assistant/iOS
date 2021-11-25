1e141f41d9669b9d27b575ab88817270a9c546d7
Zac West <zacwest@gmail.com>
2021-11-24 23:29:43 -0800

    Cleanup models when servers change

--- Sources/Shared/API/Models/ModelManager.swift
+++ Sources/Shared/API/Models/ModelManager.swift
@@ -286,5 +286,6 @@ public class ModelManager: ServerObserver {
 
     public func serversDidChange(_ serverManager: ServerManager) {
         subscribe(definitions: subscribedSubscriptions)
+        cleanup().cauterize()
     }
 }
