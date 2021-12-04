2cd08b5392b497436444b1a2d9912e3fce8e461c
Zac West <74188+zacwest@users.noreply.github.com>
2021-12-04 13:10:55 -0800

    Fix onboarding timeout when only internal URL is set and we lack SSID info (#1969)
    Fixes #1918.
    
    ## Summary
    During initial onboarding (or even after the fact), we don't have permission to read SSID values due to Location permission occurring later. When only an internal URL was set, we were falling back to homeassistant.local.:8123 which is possibly not valid, rather than using the one we slurped from discovery info.
    
    ## Any other notes
    Manually entering a URL worked here because it sets the value as 'external URL' which didn't have this issue.

--- Sources/Shared/API/ConnectionInfo.swift
+++ Sources/Shared/API/ConnectionInfo.swift
@@ -173,8 +173,17 @@ public struct ConnectionInfo: Codable, Equatable {
             activeURLType = .external
             url = externalURL
         } else {
-            activeURLType = .internal
-            url = URL(string: "http://homeassistant.local:8123")!
+            // we're missing a url, so try and fall back to one that _could_ work
+            if let remoteUIURL = remoteUIURL {
+                activeURLType = .remoteUI
+                url = remoteUIURL
+            } else if let internalURL = internalURL {
+                activeURLType = .internal
+                url = internalURL
+            } else {
+                activeURLType = .internal
+                url = URL(string: "http://homeassistant.local:8123")!
+            }
         }
 
         return url.sanitized()
@@ -214,9 +223,6 @@ public struct ConnectionInfo: Codable, Equatable {
 
     /// Returns true if current SSID is SSID marked for internal URL use.
     public var isOnInternalNetwork: Bool {
-        #if targetEnvironment(simulator)
-        return true
-        #else
         if let current = Current.connectivity.currentWiFiSSID(),
            internalSSIDs?.contains(current) == true {
             return true
@@ -228,7 +234,6 @@ public struct ConnectionInfo: Codable, Equatable {
         }
 
         return false
-        #endif
     }
 }
 
