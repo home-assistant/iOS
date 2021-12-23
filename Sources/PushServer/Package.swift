b834407628bc3f9d6e32edbed5e86d354613ecdc
Zac West <zacwest@gmail.com>
2021-12-22 21:50:24 -0800

    Relocate SharedPush and make docker work

--- Sources/PushServer/Package.swift
+++ Sources/PushServer/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/apns.git", from: "2.0.0"),
-        .package(name: "SharedPush", path: "../SharedPush"),
+        .package(name: "SharedPush", path: "SharedPush"),
     ],
     targets: [
         .target(
