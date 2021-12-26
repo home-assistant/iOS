7e5e3d58cd3bd1942c3c92e45721fec500e8866e
Zac West <zacwest@gmail.com>
2021-12-25 17:33:30 -0800

    Redirect /

--- Sources/PushServer/Sources/App/routes.swift
+++ Sources/PushServer/Sources/App/routes.swift
@@ -1,6 +1,10 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    app.get { req in
+        req.redirect(to: "https://companion.home-assistant.io")
+    }
+
     app.group("push") { push in
         let pushTopic = Environment.get("APNS_TOPIC") ?? "io.robbie.HomeAssistant"
         let pushController = PushController(appIdPrefix: pushTopic)
