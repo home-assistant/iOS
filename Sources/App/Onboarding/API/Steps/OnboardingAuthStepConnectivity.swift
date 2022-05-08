3e1dcd22f2720df7d8f867c759772e4591722231
Zac West <74188+zacwest@users.noreply.github.com>
2022-05-08 00:03:23 -0700

    Improve SSL connection errors in onboarding (#2123)
    Evaluates the certificate directly to try and get better error messages from SSL errors.

--- Sources/App/Onboarding/API/Steps/OnboardingAuthStepConnectivity.swift
+++ Sources/App/Onboarding/API/Steps/OnboardingAuthStepConnectivity.swift
@@ -29,7 +29,35 @@ struct OnboardingAuthStepConnectivity: OnboardingAuthPreStep {
 
                 let errorKind: OnboardingAuthError.ErrorKind? = {
                     switch challenge.protectionSpace.authenticationMethod {
-                    case NSURLAuthenticationMethodServerTrust: return nil
+                    case NSURLAuthenticationMethodServerTrust:
+                        guard let secTrust = challenge.protectionSpace.serverTrust else {
+                            // weird stuff is abound
+                            return nil
+                        }
+
+                        var error: CFError?
+                        let isTrusted = SecTrustEvaluateWithError(secTrust, &error)
+
+                        guard !isTrusted, let error = error as Error? else {
+                            // continue normally
+                            return nil
+                        }
+
+                        Current.Log.error("received SSL error: \((error as NSError).debugDescription)")
+
+                        var errors = [Error]()
+                        errors.append(error)
+
+                        if let underlying = (error as NSError).userInfo[NSUnderlyingErrorKey] as? Error {
+                            // higher-level error is like:
+                            // > “fake.example.com” certificate is not trusted
+                            // underlying error is like:
+                            // > “fake.example.com” has errors: SSL hostname does not match name(s) in certificate,
+                            // > Extended key usage does not match certificate usage, Root is not trusted;
+                            errors.append(underlying)
+                        }
+
+                        return .sslUntrusted(errors)
                     case NSURLAuthenticationMethodHTTPBasic: return .basicAuth
                     case NSURLAuthenticationMethodClientCertificate:
                         clientCertificateErrorOccurred = true
@@ -62,7 +90,7 @@ struct OnboardingAuthStepConnectivity: OnboardingAuthPreStep {
                     switch underlying.code {
                     case .serverCertificateUntrusted, .serverCertificateHasUnknownRoot, .serverCertificateHasBadDate,
                          .serverCertificateNotYetValid:
-                        kind = .sslUntrusted(underlying)
+                        kind = .sslUntrusted([underlying])
                     default:
                         kind = .other(underlying)
                     }
