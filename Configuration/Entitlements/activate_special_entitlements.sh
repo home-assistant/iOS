b6987d28826620ba6c4a189df34c77b40f9c4b17
Zac West <zacwest@gmail.com>
2022-10-12 21:49:46 -0700

    Fix iOS App Store by not enabling device name in Push Provider

--- Configuration/Entitlements/activate_special_entitlements.sh
+++ Configuration/Entitlements/activate_special_entitlements.sh
@@ -19,10 +19,12 @@ else
     echo "warning: Push provider disabled"
 fi
 
-if [[ $CI && $CONFIGURATION != "Release" ]]; then
-  echo "warning: Device name disabled for CI"
-elif [[ ${ENABLE_DEVICE_NAME} -eq 1 ]]; then
-    /usr/libexec/PlistBuddy -c "add com.apple.developer.device-information.user-assigned-device-name bool true" "$ENTITLEMENTS_FILE"
-else
-    echo "warning: Device name disabled"
+if [[ $TARGET_NAME = "App" ]]; then
+  if [[ $CI && $CONFIGURATION != "Release" ]]; then
+    echo "warning: Device name disabled for CI"
+  elif [[ ${ENABLE_DEVICE_NAME} -eq 1 && $FULL_PRODUCT_NAME = "" ]]; then
+      /usr/libexec/PlistBuddy -c "add com.apple.developer.device-information.user-assigned-device-name bool true" "$ENTITLEMENTS_FILE"
+  else
+      echo "warning: Device name disabled"
+  fi
 fi
