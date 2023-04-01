b1594469960ff5fc068921aecf606a133e8aad9b
Bram Kragten <mail@bramkragten.nl>
2023-04-01 21:18:07 +0200

    Add edge swipe to show sidebar (#2328)
    <!-- Thank you for submitting a Pull Request and helping to improve Home
    Assistant. Please complete the following sections to help the processing
    and review of your changes. Please do not delete anything from this
    template. -->
    
    ## Summary
    Add edge swipe to show sidebar
    
    ## Screenshots
    <!-- If this is a user-facing change not in the frontend, please include
    screenshots in light and dark mode. -->
    
    ## Link to pull request in Documentation repository
    <!-- Pull requests that add, change or remove functionality must have a
    corresponding pull request in the Companion App Documentation repository
    (https://github.com/home-assistant/companion.home-assistant). Please add
    the number of this pull request after the "#" -->
    Documentation: home-assistant/companion.home-assistant#
    
    ## Any other notes
    <!-- If there is any other information of note, like if this Pull
    Request is part of a bigger change, please include it here. -->
    
    ---------
    
    Co-authored-by: Zac West <zacwest@gmail.com>

--- Sources/Shared/Environment/Constants.swift
+++ Sources/Shared/Environment/Constants.swift
@@ -149,6 +149,7 @@ public extension Version {
     static var updateLocationGPSOptional: Version = .init(major: 2022, minor: 2, prerelease: "any0")
     static var fullWebhookSecretKey: Version = .init(major: 2022, minor: 3)
     static var conversationWebhook: Version = .init(major: 2023, minor: 2, prerelease: "any0")
+    static var externalBusCommandSidebar: Version = .init(major: 2023, minor: 4, prerelease: "b3")
 
     var coreRequiredString: String {
         L10n.requiresVersion(String(format: "core-%d.%d", major, minor ?? -1))
