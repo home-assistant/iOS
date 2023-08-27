fc8d89d6d5555f83a273ad0b24f081a6076f7061
Bruno Pantaleão <bruno.ing879@gmail.com>
2023-08-27 17:59:11 +0200

    Add ability to launch assist from complication and stop recording on silence

---
+++ Sources/Extensions/Watch/Assist/Views/AssistMicRowController.swift
@@ -0,0 +1,37 @@
+import Foundation
+import WatchKit
+
+enum AssistMicRowControllerStates {
+    case standard, loading, inProgress
+}
+
+final class AssistMicRowController: NSObject {
+    @IBOutlet private var button: WKInterfaceButton!
+    var action: (() -> Void)?
+
+    static var rowType: String {
+        "AssistMicRowController"
+    }
+
+    @IBAction func didTapMic() {
+        action?()
+    }
+
+    func updateState(_ newState: AssistMicRowControllerStates) {
+        var newImageName = ""
+
+        switch newState {
+        case .standard:
+            newImageName = "mic.circle.fill"
+            button.setEnabled(true)
+        case .loading:
+            newImageName = "ellipsis.circle.fill"
+            button.setEnabled(false)
+        case .inProgress:
+            newImageName = "stop.circle"
+            button.setEnabled(true)
+        }
+
+        button.setBackgroundImage(UIImage(systemName: newImageName))
+    }
+}
