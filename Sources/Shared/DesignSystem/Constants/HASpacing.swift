072af4e6c97918041d3e78d9216c7e7237ab7d4a
Bruno Pantaleão <bruno.ing879@gmail.com>
2023-12-06 09:36:02 -0300

    Use camel case for spacing values

--- Sources/Shared/DesignSystem/Constants/HASpacing.swift
+++ Sources/Shared/DesignSystem/Constants/HASpacing.swift
@@ -2,14 +2,14 @@ import Foundation
 
 public enum Spacing {
     public static let one: CGFloat = 8
-    public static let one_and_a_half: CGFloat = 12
+    public static let oneAndHalf: CGFloat = 12
     public static let two: CGFloat = 16
-    public static let two_and_a_half: CGFloat = 20
+    public static let twoAndHalf: CGFloat = 20
     public static let three: CGFloat = 24
-    public static let three_and_a_half: CGFloat = 28
+    public static let threeAndHalf: CGFloat = 28
     public static let four: CGFloat = 32
-    public static let four_and_a_half: CGFloat = 36
+    public static let fourAndHalf: CGFloat = 36
     public static let five: CGFloat = 40
-    public static let five_and_a_half: CGFloat = 44
+    public static let fiveAndHalf: CGFloat = 44
     public static let six: CGFloat = 48
 }
