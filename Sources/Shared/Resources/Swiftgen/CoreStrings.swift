5f2a05773291bff531d1190b349fd6f1af19014a
Bruno Pantaleão <bruno.ing879@gmail.com>
2024-01-03 22:51:06 +0100

    lint

--- Sources/Shared/Resources/Swiftgen/CoreStrings.swift
+++ Sources/Shared/Resources/Swiftgen/CoreStrings.swift
@@ -10,24 +10,24 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 public enum CoreStrings {
-  /// Button
-  public static var componentButtonTitle: String { return CoreStrings.tr("Core", "component::button::title") }
-  /// Cover
-  public static var componentCoverTitle: String { return CoreStrings.tr("Core", "component::cover::title") }
-  /// Input boolean
-  public static var componentInputBooleanTitle: String { return CoreStrings.tr("Core", "component::input_boolean::title") }
-  /// Input button
-  public static var componentInputButtonTitle: String { return CoreStrings.tr("Core", "component::input_button::title") }
-  /// Light
-  public static var componentLightTitle: String { return CoreStrings.tr("Core", "component::light::title") }
-  /// Lock
-  public static var componentLockTitle: String { return CoreStrings.tr("Core", "component::lock::title") }
-  /// Scene
-  public static var componentSceneTitle: String { return CoreStrings.tr("Core", "component::scene::title") }
-  /// Script
-  public static var componentScriptTitle: String { return CoreStrings.tr("Core", "component::script::title") }
-  /// Switch
-  public static var componentSwitchTitle: String { return CoreStrings.tr("Core", "component::switch::title") }
+    /// Button
+    public static var componentButtonTitle: String { return CoreStrings.tr("Core", "component::button::title") }
+    /// Cover
+    public static var componentCoverTitle: String { return CoreStrings.tr("Core", "component::cover::title") }
+    /// Input boolean
+    public static var componentInputBooleanTitle: String { return CoreStrings.tr("Core", "component::input_boolean::title") }
+    /// Input button
+    public static var componentInputButtonTitle: String { return CoreStrings.tr("Core", "component::input_button::title") }
+    /// Light
+    public static var componentLightTitle: String { return CoreStrings.tr("Core", "component::light::title") }
+    /// Lock
+    public static var componentLockTitle: String { return CoreStrings.tr("Core", "component::lock::title") }
+    /// Scene
+    public static var componentSceneTitle: String { return CoreStrings.tr("Core", "component::scene::title") }
+    /// Script
+    public static var componentScriptTitle: String { return CoreStrings.tr("Core", "component::script::title") }
+    /// Switch
+    public static var componentSwitchTitle: String { return CoreStrings.tr("Core", "component::switch::title") }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -35,8 +35,8 @@ public enum CoreStrings {
 // MARK: - Implementation Details
 
 extension CoreStrings {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = Current.localized.string(key, table)
-    return String(format: format, locale: Locale.current, arguments: args)
-  }
+    private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+        let format = Current.localized.string(key, table)
+        return String(format: format, locale: Locale.current, arguments: args)
+    }
 }
--- Sources/Shared/Resources/Swiftgen/FrontendStrings.swift
+++ Sources/Shared/Resources/Swiftgen/FrontendStrings.swift
@@ -10,30 +10,30 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 public enum FrontendStrings {
-  /// Calendar
-  public static var panelCalendar: String { return FrontendStrings.tr("Frontend", "panel::calendar") }
-  /// Settings
-  public static var panelConfig: String { return FrontendStrings.tr("Frontend", "panel::config") }
-  /// Developer tools
-  public static var panelDeveloperTools: String { return FrontendStrings.tr("Frontend", "panel::developer_tools") }
-  /// Energy
-  public static var panelEnergy: String { return FrontendStrings.tr("Frontend", "panel::energy") }
-  /// History
-  public static var panelHistory: String { return FrontendStrings.tr("Frontend", "panel::history") }
-  /// Logbook
-  public static var panelLogbook: String { return FrontendStrings.tr("Frontend", "panel::logbook") }
-  /// Mailbox
-  public static var panelMailbox: String { return FrontendStrings.tr("Frontend", "panel::mailbox") }
-  /// Map
-  public static var panelMap: String { return FrontendStrings.tr("Frontend", "panel::map") }
-  /// Media
-  public static var panelMediaBrowser: String { return FrontendStrings.tr("Frontend", "panel::media_browser") }
-  /// Profile
-  public static var panelProfile: String { return FrontendStrings.tr("Frontend", "panel::profile") }
-  /// Shopping list
-  public static var panelShoppingList: String { return FrontendStrings.tr("Frontend", "panel::shopping_list") }
-  /// Overview
-  public static var panelStates: String { return FrontendStrings.tr("Frontend", "panel::states") }
+    /// Calendar
+    public static var panelCalendar: String { return FrontendStrings.tr("Frontend", "panel::calendar") }
+    /// Settings
+    public static var panelConfig: String { return FrontendStrings.tr("Frontend", "panel::config") }
+    /// Developer tools
+    public static var panelDeveloperTools: String { return FrontendStrings.tr("Frontend", "panel::developer_tools") }
+    /// Energy
+    public static var panelEnergy: String { return FrontendStrings.tr("Frontend", "panel::energy") }
+    /// History
+    public static var panelHistory: String { return FrontendStrings.tr("Frontend", "panel::history") }
+    /// Logbook
+    public static var panelLogbook: String { return FrontendStrings.tr("Frontend", "panel::logbook") }
+    /// Mailbox
+    public static var panelMailbox: String { return FrontendStrings.tr("Frontend", "panel::mailbox") }
+    /// Map
+    public static var panelMap: String { return FrontendStrings.tr("Frontend", "panel::map") }
+    /// Media
+    public static var panelMediaBrowser: String { return FrontendStrings.tr("Frontend", "panel::media_browser") }
+    /// Profile
+    public static var panelProfile: String { return FrontendStrings.tr("Frontend", "panel::profile") }
+    /// Shopping list
+    public static var panelShoppingList: String { return FrontendStrings.tr("Frontend", "panel::shopping_list") }
+    /// Overview
+    public static var panelStates: String { return FrontendStrings.tr("Frontend", "panel::states") }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -41,8 +41,8 @@ public enum FrontendStrings {
 // MARK: - Implementation Details
 
 extension FrontendStrings {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = Current.localized.string(key, table)
-    return String(format: format, locale: Locale.current, arguments: args)
-  }
+    private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+        let format = Current.localized.string(key, table)
+        return String(format: format, locale: Locale.current, arguments: args)
+    }
 }
