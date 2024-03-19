2d7457c65c8398aa75752a62fb9c07690974a15e
Michal Srutek <michal.srutek@outlook.com>
2024-03-19 10:04:17 +0100

    Fix format

--- Sources/Shared/Common/Extensions/String+HA.swift
+++ Sources/Shared/Common/Extensions/String+HA.swift
@@ -2,12 +2,12 @@ import CoreGraphics
 import Foundation
 import UIKit
 
-extension String {
-    public var djb2hash: Int {
+public extension String {
+    var djb2hash: Int {
         unicodeScalars.map(\.value).reduce(5381) { ($0 << 5) &+ $0 &+ Int($1) }
     }
 
-    public var containsJinjaTemplate: Bool {
+    var containsJinjaTemplate: Bool {
         contains("{{") || contains("{%") || contains("{#")
     }
 }
