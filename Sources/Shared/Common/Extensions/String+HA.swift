18fb24f44497c64a353aabd722f65640d0f27da1
Michal Šrůtek <35694712+michalsrutek@users.noreply.github.com>
2024-03-19 12:06:17 +0100

    🧹 Remove unused `String` extension functions (#2659)
    ## Summary
    🧹

--- Sources/Shared/Common/Extensions/String+HA.swift
+++ Sources/Shared/Common/Extensions/String+HA.swift
@@ -2,47 +2,12 @@ import CoreGraphics
 import Foundation
 import UIKit
 
-extension String {
-    public var djb2hash: Int {
+public extension String {
+    var djb2hash: Int {
         unicodeScalars.map(\.value).reduce(5381) { ($0 << 5) &+ $0 &+ Int($1) }
     }
 
-    public var containsJinjaTemplate: Bool {
+    var containsJinjaTemplate: Bool {
         contains("{{") || contains("{%") || contains("{#")
-    }
-
-    func dictionary() -> [String: Any]? {
-        if let data = data(using: .utf8) {
-            do {
-                return try JSONSerialization.jsonObject(with: data) as? [String: Any]
-            } catch {
-                print("Error serializing JSON string to dict: \(error)")
-            }
-        }
-        return nil
-    }
-
-    func colorWithHexValue(alpha: CGFloat? = 1.0) -> UIColor {
-        // Convert hex string to an integer
-        let hexint = Int(String.intFromHexString(self))
-        let red = CGFloat((hexint & 0xFF0000) >> 16) / 255.0
-        let green = CGFloat((hexint & 0xFF00) >> 8) / 255.0
-        let blue = CGFloat((hexint & 0xFF) >> 0) / 255.0
-        let alpha = alpha!
-
-        // Create color object, specifying alpha as well
-        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
-        return color
-    }
-
-    private static func intFromHexString(_ hexStr: String) -> UInt64 {
-        var hexInt: UInt64 = 0
-        // Create scanner
-        let scanner = Scanner(string: hexStr)
-        // Tell scanner to skip the # character
-        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
-        // Scan hex value
-        scanner.scanHexInt64(&hexInt)
-        return hexInt
     }
 }
