adb88c9b82d75dac09407144f99d9cd57df214d5
Bruno Pantaleão Gonçalves <bruno.ing879@gmail.com>
2024-04-30 14:00:28 +0200

    Avoid invoking entire realm for action intents

--- Sources/Shared/Intents/WidgetActionsIntentHandler.swift
+++ Sources/Shared/Intents/WidgetActionsIntentHandler.swift
@@ -1,13 +1,15 @@
 import Foundation
 import Intents
 import PromiseKit
+import RealmSwift
 
 class WidgetActionsIntentHandler: NSObject, WidgetActionsIntentHandling {
     func provideActionsOptionsCollection(
         for intent: WidgetActionsIntent,
         with completion: @escaping (INObjectCollection<IntentAction>?, Error?) -> Void
     ) {
-        let actions = Current.realm().objects(Action.self).sorted(byKeyPath: #keyPath(Action.Position))
+        let actions = Realm.getRealm(objectTypes: [Action.self]).objects(Action.self)
+            .sorted(byKeyPath: #keyPath(Action.Position))
         let performActions = Array(actions.map { IntentAction(action: $0) })
         completion(.init(items: performActions), nil)
     }
