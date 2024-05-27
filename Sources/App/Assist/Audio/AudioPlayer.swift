049efb4b846397d8bc97075eed91c86c9c109d3d
Bruno Pantaleão <bruno.ing879@gmail.com>
2024-05-27 10:29:45 +0200

    Fix Assist audio player audio session setup

--- Sources/App/Assist/Audio/AudioPlayer.swift
+++ Sources/App/Assist/Audio/AudioPlayer.swift
@@ -1,5 +1,6 @@
 import AVFoundation
 import Foundation
+import Shared
 
 protocol AudioPlayerProtocol {
     func play(url: URL)
@@ -10,6 +11,15 @@ final class AudioPlayer: NSObject, AudioPlayerProtocol {
     private let player = AVPlayer()
 
     func play(url: URL) {
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setActive(false)
+            try audioSession.setCategory(.playback)
+            try audioSession.setActive(true)
+        } catch {
+            Current.Log.error("Failed to setup audio session for audio player: \(error.localizedDescription)")
+        }
+
         let playerItem = AVPlayerItem(url: url)
         player.replaceCurrentItem(with: playerItem)
         player.play()
