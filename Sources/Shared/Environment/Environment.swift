00b225ea754855dad6f547ec7e672f763fa9533f
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2024-08-02 11:57:35 +0200

    Remove App Version as sensor

--- Sources/Shared/Environment/Environment.swift
+++ Sources/Shared/Environment/Environment.swift
@@ -148,7 +148,6 @@ public class AppEnvironment {
         $0.register(provider: FocusSensor.self)
         $0.register(provider: LastUpdateSensor.self)
         $0.register(provider: WatchBatterySensor.self)
-        $0.register(provider: AppVersionSensor.self)
         $0.register(provider: LocationPermissionSensor.self)
     }
 
