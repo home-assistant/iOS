6296c70989a02ac5a9369b7f63a3496eb7a1e1ec
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2024-12-12 15:01:09 +0100

    Adjust sensors widget value to the correct precision

--- Sources/Extensions/AppIntents/Widget/Sensor/WidgetSensorsAppIntentTimelineProvider.swift
+++ Sources/Extensions/AppIntents/Widget/Sensor/WidgetSensorsAppIntentTimelineProvider.swift
@@ -123,7 +123,12 @@ struct WidgetSensorsAppIntentTimelineProvider: AppIntentTimelineProvider {
             throw WidgetSensorsDataError.badResponse
         }
 
-        let stateValue = (state?["state"] as? String) ?? "N/A"
+        var stateValue = (state?["state"] as? String) ?? "N/A"
+        stateValue = adjustPrecision(
+            serverId: server.identifier.rawValue,
+            entityId: sensor.entityId,
+            stateValue: stateValue
+        )
         let unitOfMeasurement = (state?["attributes"] as? [String: Any])?["unit_of_measurement"] as? String
 
         return WidgetSensorsEntry.SensorData(
@@ -133,6 +138,39 @@ struct WidgetSensorsAppIntentTimelineProvider: AppIntentTimelineProvider {
             unitOfMeasurement: unitOfMeasurement,
             icon: sensor.icon
         )
+    }
+
+    private func adjustPrecision(serverId: String, entityId: String, stateValue: String) -> String {
+        guard var stateValueFloat = Float(stateValue) else {
+            return stateValue
+        }
+        if var decimalPlacesForEntityId: Int = {
+            do {
+                return try Current.database.read { db in
+                    try AppEntityRegistryListForDisplay
+                        .filter(
+                            Column(DatabaseTables.AppEntityRegistryListForDisplay.id.rawValue) == ServerEntity
+                                .uniqueId(
+                                    serverId: serverId,
+                                    entityId: entityId
+                                )
+                        )
+                        .fetchOne(db)?.registry.decimalPlaces
+                }
+            } catch {
+                Current.Log.error("Failed to fetch decimal places for entity ID: \(entityId)")
+                return nil
+            }
+        }() {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            numberFormatter.locale = Locale.current
+            numberFormatter.maximumFractionDigits = decimalPlacesForEntityId
+            numberFormatter.minimumFractionDigits = decimalPlacesForEntityId
+            return numberFormatter.string(from: NSNumber(value: stateValueFloat)) ?? stateValue
+        } else {
+            return stateValue
+        }
     }
 
     private func suggestions() async -> [Server: [HAAppEntity]] {
