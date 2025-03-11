850ffaa00c22875e85c83736f854e4f54c2ba26a
Fábio Oliveira <banaslee@gmail.com>
2025-03-11 18:31:32 +0000

    Apply snapshotting technique to Widgets

--- Sources/Extensions/Widgets/Common/WidgetBasicContainerView.swift
+++ Sources/Extensions/Widgets/Common/WidgetBasicContainerView.swift
@@ -34,6 +34,139 @@ struct WidgetBasicContainerView: View {
     }
 }
 
+@available(iOS 18, *)
+struct WidgetBasicContainerView_Previews: PreviewProvider {
+	struct WidgetBasicContainerViewPreviewData {
+		let modelsCount: Int
+		let withSubtitle: Bool
+		let withIconBackgroundColor: Bool
+	}
+	
+	static var previews: some View {
+		WidgetBasicContainerView_Previews.systemSmallConfigurations.previews()
+		WidgetBasicContainerView_Previews.systemMediumConfigurations.previews()
+		WidgetBasicContainerView_Previews.systemLargeConfigurations.previews()
+	}
+	
+	static var systemSmallConfigurations: SnapshottablePreviewConfigurations<WidgetBasicContainerViewPreviewData> = .init(
+		configurations: Self.configurations(for: .systemSmall)
+	) { previewData in
+		widgetBasicContainerView(
+			modelsCount: previewData.modelsCount,
+			withSubtitle: previewData.withSubtitle,
+			withIconBackgroundColor: previewData.withIconBackgroundColor,
+			familySize: .systemSmall
+		)
+		.previewContext(WidgetPreviewContext(family: WidgetFamily.systemSmall))
+	}
+	
+	static var systemMediumConfigurations: SnapshottablePreviewConfigurations<WidgetBasicContainerViewPreviewData> = .init(
+		configurations: Self.configurations(for: .systemMedium)
+	) { previewData in
+		widgetBasicContainerView(
+			modelsCount: previewData.modelsCount,
+			withSubtitle: previewData.withSubtitle,
+			withIconBackgroundColor: previewData.withIconBackgroundColor,
+			familySize: .systemMedium
+		)
+		.previewContext(WidgetPreviewContext(family: WidgetFamily.systemMedium))
+	}
+	
+	static var systemLargeConfigurations: SnapshottablePreviewConfigurations<WidgetBasicContainerViewPreviewData> = .init(
+		configurations: Self.configurations(for: .systemLarge)
+	) { previewData in
+		widgetBasicContainerView(
+			modelsCount: previewData.modelsCount,
+			withSubtitle: previewData.withSubtitle,
+			withIconBackgroundColor: previewData.withIconBackgroundColor,
+			familySize: .systemLarge
+		)
+		.previewContext(WidgetPreviewContext(family: WidgetFamily.systemLarge))
+	}
+	
+	private static func maxTiles(for familySize: WidgetFamily) -> Int {
+		return switch familySize {
+			case .systemSmall: 3
+			case .systemMedium: 6
+			case .systemLarge: 12
+			default: 12
+		}
+	}
+	
+	private static func configurations(for familySize: WidgetFamily) -> [SnapshottablePreviewConfigurations<WidgetBasicContainerViewPreviewData>.Configuration<WidgetBasicContainerViewPreviewData>] {
+		(1 ... maxTiles(for: familySize))
+			.flatMap { maxTiles in
+				[
+					.init(
+						item: .init(
+							modelsCount: maxTiles,
+							withSubtitle: true,
+							withIconBackgroundColor: true
+						),
+						name: "withSubtitleWithIconBackground-\(familySize.description)-\(maxTiles)_tiles"
+					),
+					.init(
+						item: .init(
+							modelsCount: maxTiles,
+							withSubtitle: true,
+							withIconBackgroundColor: false
+						),
+						name: "withSubtitleWithoutIconBackground-\(familySize.description)-\(maxTiles)_tiles"
+					),
+					.init(
+						item: .init(
+							modelsCount: maxTiles,
+							withSubtitle: false,
+							withIconBackgroundColor: true
+						),
+						name: "withoutSubtitleWithIconBackground-\(familySize.description)-\(maxTiles)_tiles"
+					),
+					.init(
+						item: .init(
+							modelsCount: maxTiles,
+							withSubtitle: false,
+							withIconBackgroundColor: false
+						),
+						name: "withoutSubtitleWithoutIconBackground-\(familySize.description)-\(maxTiles)_tiles"
+					)
+				]
+			}
+	}
+	
+	private static func widgetBasicContainerView(
+		modelsCount: Int,
+		withSubtitle: Bool,
+		withIconBackgroundColor: Bool,
+		familySize: WidgetFamily
+	) -> some View {
+		WidgetBasicContainerWrapperView(
+			emptyViewGenerator: {
+				AnyView(EmptyView())
+			},
+			contents: models(
+				count: modelsCount,
+				withSubtitle: withSubtitle,
+				withIconBackgroundColor: withIconBackgroundColor
+			),
+			type: .custom,
+			family: familySize
+		)
+	}
+	
+	private static func models(count: Int, withSubtitle: Bool, withIconBackgroundColor: Bool) -> [WidgetBasicViewModel] {
+		(0 ..< count).map { index in
+			WidgetBasicViewModel(
+				id: "\(index)",
+				title: "Title \(index)",
+				subtitle: withSubtitle ? "Subtitle \(index)" : nil,
+				interactionType: .appIntent(.refresh),
+				icon: .abTestingIcon,
+				showIconBackground: withIconBackgroundColor
+			)
+		}
+	}
+}
+
 /// This wrapper only exists so it can be snapshot tested with the proper family size which is not possible with the
 /// `WidgetBasicContainerView` and the environment variable
 struct WidgetBasicContainerWrapperView: View {
--- Tests/Widgets/WidgetsSnapshot.test.swift
+++ Tests/Widgets/WidgetsSnapshot.test.swift
@@ -1,5 +1,9 @@
 @testable import HomeAssistant
+
+import SharedTesting
+
 import SnapshotTesting
+
 import SwiftUI
 import Testing
 import WidgetKit
@@ -7,91 +11,38 @@ import WidgetKit
 /* Some snapshots may look duplicate but they guarantee that if we pass more models to a widget container
  it will only displays what it's family supports guided by `WidgetFamilySizes` */
 struct WidgetsSnapshotTests {
-    private let maxNumberOfTiles = 12
-
-    @available(iOS 18, *)
-    @MainActor @Test func snapshotSingleTileWithSubtitleAndWithIconBackgroundTest() {
-        for familySize in WidgetCustomSupportedFamilies.families {
-            for i in 0 ..< maxNumberOfTiles {
-                let view = widgetBasicContainerView(
-                    modelsCount: i + 1,
-                    withSubtitle: true,
-                    withIconBackgroundColor: true,
-                    familySize: familySize
-                )
-                assertSnapshot(
-                    of: view,
-                    as: .image(layout: .fixed(
-                        width: widthForPreview(family: familySize),
-                        height: heightForPreview(family: familySize)
-                    ))
-                )
-            }
-        }
-    }
-
-    @available(iOS 18, *)
-    @MainActor @Test func snapshotSingleTileWithSubtitleAndWithoutIconBackgroundTest() {
-        for familySize in WidgetCustomSupportedFamilies.families {
-            for i in 0 ..< maxNumberOfTiles {
-                let view = widgetBasicContainerView(
-                    modelsCount: i + 1,
-                    withSubtitle: true,
-                    withIconBackgroundColor: false,
-                    familySize: familySize
-                )
-                assertSnapshot(
-                    of: view,
-                    as: .image(layout: .fixed(
-                        width: widthForPreview(family: familySize),
-                        height: heightForPreview(family: familySize)
-                    ))
-                )
-            }
-        }
-    }
-
-    @available(iOS 18, *)
-    @MainActor @Test func snapshotSingleTileWithoutSubtitleAndWithIconBackgroundTest() {
-        for familySize in WidgetCustomSupportedFamilies.families {
-            for i in 0 ..< maxNumberOfTiles {
-                let view = widgetBasicContainerView(
-                    modelsCount: i + 1,
-                    withSubtitle: false,
-                    withIconBackgroundColor: true,
-                    familySize: familySize
-                )
-                assertSnapshot(
-                    of: view,
-                    as: .image(layout: .fixed(
-                        width: widthForPreview(family: familySize),
-                        height: heightForPreview(family: familySize)
-                    ))
-                )
-            }
-        }
-    }
-
-    @available(iOS 18, *)
-    @MainActor @Test func snapshotSingleTileWithoutSubtitleAndWithoutIconBackgroundTest() {
-        for familySize in WidgetCustomSupportedFamilies.families {
-            for i in 0 ..< maxNumberOfTiles {
-                let view = widgetBasicContainerView(
-                    modelsCount: i + 1,
-                    withSubtitle: false,
-                    withIconBackgroundColor: false,
-                    familySize: familySize
-                )
-                assertSnapshot(
-                    of: view,
-                    as: .image(layout: .fixed(
-                        width: widthForPreview(family: familySize),
-                        height: heightForPreview(family: familySize)
-                    ))
-                )
-            }
-        }
-    }
+	@available(iOS 18, *)
+	@MainActor @Test func systemLargeSnapshots() {
+		WidgetBasicContainerView_Previews.systemLargeConfigurations.assertSnapshots(
+			layout: .fixed(
+				width: widthForPreview(family: .systemLarge),
+				height: heightForPreview(family: .systemLarge)
+			),
+			traits: .init()
+		)
+	}
+	
+	@available(iOS 18, *)
+	@MainActor @Test func systemMediumSnapshots() {
+		WidgetBasicContainerView_Previews.systemMediumConfigurations.assertSnapshots(
+			layout: .fixed(
+				width: widthForPreview(family: .systemMedium),
+				height: heightForPreview(family: .systemMedium)
+			),
+			traits: .init()
+		)
+	}
+	
+	@available(iOS 18, *)
+	@MainActor @Test func systemSmallSnapshots() {
+		WidgetBasicContainerView_Previews.systemSmallConfigurations.assertSnapshots(
+			layout: .fixed(
+				width: widthForPreview(family: .systemSmall),
+				height: heightForPreview(family: .systemSmall)
+			),
+			traits: .init()
+		)
+	}
 
     private func heightForPreview(family: WidgetFamily) -> CGFloat {
         switch family {
@@ -112,39 +63,6 @@ struct WidgetsSnapshotTests {
             return 350
         default:
             return 600
-        }
-    }
-
-    private func widgetBasicContainerView(
-        modelsCount: Int,
-        withSubtitle: Bool,
-        withIconBackgroundColor: Bool,
-        familySize: WidgetFamily
-    ) -> some View {
-        WidgetBasicContainerWrapperView(
-            emptyViewGenerator: {
-                AnyView(EmptyView())
-            },
-            contents: models(
-                count: modelsCount,
-                withSubtitle: withSubtitle,
-                withIconBackgroundColor: withIconBackgroundColor
-            ),
-            type: .custom,
-            family: familySize
-        )
-    }
-
-    private func models(count: Int, withSubtitle: Bool, withIconBackgroundColor: Bool) -> [WidgetBasicViewModel] {
-        (0 ..< count).map { index in
-            WidgetBasicViewModel(
-                id: "\(index)",
-                title: "Title \(index)",
-                subtitle: withSubtitle ? "Subtitle \(index)" : nil,
-                interactionType: .appIntent(.refresh),
-                icon: .abTestingIcon,
-                showIconBackground: withIconBackgroundColor
-            )
         }
     }
 }
