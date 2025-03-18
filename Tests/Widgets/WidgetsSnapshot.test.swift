3c963f547bfd074db49a09c3609072ca131c0bc3
Fábio Oliveira <banaslee@gmail.com>
2025-03-18 13:27:43 +0000

    Simplify widget snapshot size calculation

--- Tests/Widgets/WidgetsSnapshot.test.swift
+++ Tests/Widgets/WidgetsSnapshot.test.swift
@@ -8,53 +8,53 @@ import WidgetKit
 struct WidgetsSnapshotTests {
     @available(iOS 18, *)
     @MainActor @Test func systemLargeSnapshots() {
-        WidgetBasicContainerView_Previews.systemLargeConfigurations.assertLightDarkSnapshots(
-            layout: .fixed(
-                width: widthForPreview(family: .systemLarge),
-                height: heightForPreview(family: .systemLarge)
+        let size = snapshotSize(for: .systemLarge)
+        WidgetBasicContainerView_Previews
+            .systemLargeConfigurations
+            .assertLightDarkSnapshots(
+                layout: .fixed(
+                    width: size.width,
+                    height: size.height
+                )
             )
-        )
     }
 
     @available(iOS 18, *)
     @MainActor @Test func systemMediumSnapshots() {
-        WidgetBasicContainerView_Previews.systemMediumConfigurations.assertLightDarkSnapshots(
-            layout: .fixed(
-                width: widthForPreview(family: .systemMedium),
-                height: heightForPreview(family: .systemMedium)
+        let size = snapshotSize(for: .systemLarge)
+        WidgetBasicContainerView_Previews
+            .systemMediumConfigurations
+            .assertLightDarkSnapshots(
+                layout: .fixed(
+                    width: size.width,
+                    height: size.height
+                )
             )
-        )
     }
 
     @available(iOS 18, *)
     @MainActor @Test func systemSmallSnapshots() {
-        WidgetBasicContainerView_Previews.systemSmallConfigurations.assertLightDarkSnapshots(
-            layout: .fixed(
-                width: widthForPreview(family: .systemSmall),
-                height: heightForPreview(family: .systemSmall)
+        let size = snapshotSize(for: .systemLarge)
+        WidgetBasicContainerView_Previews
+            .systemSmallConfigurations
+            .assertLightDarkSnapshots(
+                layout: .fixed(
+                    width: size.width,
+                    height: size.height
+                )
             )
-        )
     }
 
-    private func heightForPreview(family: WidgetFamily) -> CGFloat {
-        switch family {
-        case .systemSmall, .systemMedium:
-            return 160
-        case .systemLarge:
-            return 310
-        default:
-            return 600
-        }
-    }
-
-    private func widthForPreview(family: WidgetFamily) -> CGFloat {
+    private func snapshotSize(for family: WidgetFamily) -> CGSize {
         switch family {
         case .systemSmall:
-            return 160
-        case .systemMedium, .systemLarge:
-            return 350
+            CGSize(width: 160, height: 160)
+        case .systemMedium:
+            CGSize(width: 350, height: 160)
+        case .systemLarge:
+            CGSize(width: 350, height: 310)
         default:
-            return 600
+            CGSize(width: 600, height: 600)
         }
     }
 }
