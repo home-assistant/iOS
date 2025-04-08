2bb118a368f97023daf4ed29d189f5edf26aca3e
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2025-04-08 22:53:31 +0200

    Add onboarding navigation tests

---
+++ Tests/App/Onboarding/OnboardingNavigationTests.swift
@@ -0,0 +1,11 @@
+@testable import HomeAssistant
+@testable import Shared
+import Testing
+
+struct OnboardingNavigationTests {
+    @Test func testOnboardingNavigationWhenNoServers() async throws {
+        Current.servers = FakeServerManager(initial: 0)
+        let result = OnboardingNavigation.requiredOnboardingStyle
+        assert(result == .required(.full))
+    }
+}
