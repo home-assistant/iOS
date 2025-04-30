ec31be2f465fcc388287c64c3b756307a4d1689f
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2025-04-30 15:16:09 +0200

    Lint

--- Tests/App/Settings/SensorRow.test.swift
+++ Tests/App/Settings/SensorRow.test.swift
@@ -1,23 +1,21 @@
-//
-//  SettingsSensorsTests.swift
-//  Tests-App
-//
-//  Created by Bruno Pantaleão on 30/4/25.
-//  Copyright © 2025 Home Assistant. All rights reserved.
-//
-
 @testable import HomeAssistant
 @testable import Shared
-import Testing
 import SwiftUI
+import Testing
 
 struct SensorRowTests {
-
     @MainActor
     @Test func testRowView() async throws {
         let view = List {
             SensorRow(
-                sensor: WebhookSensor(name: "Sensor 1", uniqueID: "1", icon: .abTestingIcon, state: false, unit: nil, entityCategory: nil),
+                sensor: WebhookSensor(
+                    name: "Sensor 1",
+                    uniqueID: "1",
+                    icon: .abTestingIcon,
+                    state: false,
+                    unit: nil,
+                    entityCategory: nil
+                ),
                 isEnabled: true
             )
         }
