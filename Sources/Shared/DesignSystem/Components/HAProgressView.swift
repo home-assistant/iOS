55e0386d0098beee1b8ae7081d2077893532a90b
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2025-05-22 15:12:25 +0200

    Improve servers list view loading objects and timin to display

--- Sources/Shared/DesignSystem/Components/HAProgressView.swift
+++ Sources/Shared/DesignSystem/Components/HAProgressView.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+class HAProgressViewModel: ObservableObject {
+    @Published var isAnimating = false
+    @Published var trimEnd: CGFloat = 0.1
+}
+
 public struct HAProgressView: View {
     public enum Style {
         case small
@@ -25,9 +30,7 @@ public struct HAProgressView: View {
         }
     }
 
-    @State private var isAnimating = false
-    @State private var trimEnd: CGFloat = 0.0
-
+    @StateObject private var viewModel = HAProgressViewModel()
     let style: Style
 
     public init(style: Style = .medium) {
@@ -40,18 +43,21 @@ public struct HAProgressView: View {
                 .stroke(Color.track, style: StrokeStyle(lineWidth: style.lineWidth))
                 .frame(width: style.size.width, height: style.size.height)
             Circle()
-                .trim(from: 0.0, to: trimEnd)
+                .trim(from: 0, to: viewModel.trimEnd)
                 .stroke(
                     Color.haPrimary,
                     style: StrokeStyle(lineWidth: style.lineWidth, lineCap: .round)
                 )
                 .frame(width: style.size.width, height: style.size.height)
-                .rotationEffect(Angle(degrees: isAnimating ? 360 : 0))
-                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false), value: isAnimating)
+                .rotationEffect(Angle(degrees: viewModel.isAnimating ? 360 : 0))
+                .animation(
+                    Animation.linear(duration: 1).repeatForever(autoreverses: false),
+                    value: viewModel.isAnimating
+                )
                 .onAppear {
-                    isAnimating = true
+                    viewModel.isAnimating = true
                     withAnimation(.easeInOut(duration: 1.75).repeatForever(autoreverses: true)) {
-                        trimEnd = 0.7
+                        viewModel.trimEnd = 0.7
                     }
                 }
         }
