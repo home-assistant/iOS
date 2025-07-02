d7c6c3b6f11ba91c20c06a042ce861b4ef891fbc
Silas Krause <krausesilas@gmail.com>
2025-07-02 12:07:16 +0200

    Formatting

--- Sources/Shared/Assist/AssistChatItem.swift
+++ Sources/Shared/Assist/AssistChatItem.swift
@@ -10,12 +10,20 @@ public struct AssistChatItem: Equatable {
     public var id: String = UUID().uuidString
     public let content: String
     public var markdown: AttributedString {
-        var content = (try? AttributedString(markdown: self.content, options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace, failurePolicy: .returnPartiallyParsedIfPossible))) ?? AttributedString(self.content)
+        var content = (try? AttributedString(
+            markdown: content,
+            options: AttributedString
+                .MarkdownParsingOptions(
+                    interpretedSyntax: .inlineOnlyPreservingWhitespace,
+                    failurePolicy: .returnPartiallyParsedIfPossible
+                )
+        )) ?? AttributedString(content)
         for run in content.runs where run.attributes.link != nil {
             content[run.range].underlineStyle = .single
         }
         return content
     }
+
     public let itemType: ItemType
 
     public enum ItemType {
