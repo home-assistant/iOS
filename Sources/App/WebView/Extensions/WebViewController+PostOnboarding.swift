69cb0532b025a97155181347fbb3193310cecb8f
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2025-07-09 15:18:09 +0200

    Updated notification permission UI

--- Sources/App/WebView/Extensions/WebViewController+PostOnboarding.swift
+++ Sources/App/WebView/Extensions/WebViewController+PostOnboarding.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Shared
 import SwiftMessages
+import SwiftUI
 
 // MARK: - Post onboarding
 
@@ -10,8 +11,7 @@ extension WebViewController {
         let delayedSeconds: CGFloat = 3
         DispatchQueue.main.asyncAfter(deadline: .now() + delayedSeconds) { [weak self] in
             Task {
-                let notificationCenter = Current.userNotificationCenter
-                let settings = await notificationCenter.notificationSettings()
+                let settings = await Current.userNotificationCenter.notificationSettings()
                 if ![.authorized, .denied].contains(settings.authorizationStatus) {
                     self?.showNotificationPermissionRequest()
                 }
@@ -20,36 +20,10 @@ extension WebViewController {
     }
 
     private func showNotificationPermissionRequest() {
-        let view = MessageView.viewFromNib(layout: .cardView)
-        var config = SwiftMessages.Config()
-        config.duration = .forever
-        config.presentationStyle = .top
-        config.dimMode = .gray(interactive: true)
-        view.configureContent(
-            title: L10n.PostOnboarding.Permission.Notification.title,
-            body: L10n.PostOnboarding.Permission.Notification.message,
-            iconImage: nil,
-            iconText: nil,
-            buttonImage: MaterialDesignIcons.arrowRightBoldCircleIcon.image(
-                ofSize: .init(width: 35, height: 35),
-                color: .haPrimary
-            ),
-            buttonTitle: nil,
-            buttonTapHandler: { _ in
-                SwiftMessages.hide()
-                UNUserNotificationCenter.current().requestAuthorization(options: .defaultOptions) { _, error in
-                    if let error {
-                        Current.Log.error("Error when requesting notifications permissions: \(error)")
-                    }
-                    DispatchQueue.main.async {
-                        UIApplication.shared.registerForRemoteNotifications()
-                    }
-                }
-            }
-        )
-        view.titleLabel?.numberOfLines = 0
-        view.bodyLabel?.numberOfLines = 0
-
-        SwiftMessages.show(config: config, view: view)
+        let view = NotificationPermissionRequestView().embeddedInHostingController()
+        view.modalPresentationStyle = .overFullScreen
+        view.view.backgroundColor = .clear
+        view.modalTransitionStyle = .crossDissolve
+        present(view, animated: true)
     }
 }
