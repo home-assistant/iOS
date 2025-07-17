2c5a4b5c54506cb47aaaa54ba0206f803b24c3ad
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2025-07-17 13:54:35 +0200

    Copy changes and organize permission request

--- Sources/App/Utilities/PermissionType+Onboarding.swift
+++ Sources/App/Utilities/PermissionType+Onboarding.swift
@@ -18,48 +18,64 @@ extension PermissionType {
 
     func request(_ completionHandler: @escaping (Bool, PermissionStatus) -> Void) {
         if status == .denied {
-            let destination: UIApplication.OpenSettingsDestination
-
-            switch self {
-            case .location: destination = .location
-            case .motion: destination = .motion
-            case .focus: destination = .focus
-            }
-
-            UIApplication.shared.openSettings(destination: destination, completionHandler: nil)
-            completionHandler(false, .denied)
+            handlePermissionsThatWereDeniedPreviously(completionHandler)
             return
         }
 
         switch self {
         case .location:
-            if PermissionsLocationDelegate.shared == nil {
-                PermissionsLocationDelegate.shared = PermissionsLocationDelegate()
-            }
-
-            PermissionsLocationDelegate.shared!.requestPermission { status in
-                DispatchQueue.main.async {
-                    completionHandler(status == .authorized || status == .authorizedWhenInUse, status)
-                    PermissionsLocationDelegate.shared = nil
-                }
-            }
+            handleLocationPermissionRequest(completionHandler)
         case .motion:
-            let manager = CMMotionActivityManager()
-            let now = Date()
-
-            manager.queryActivityStarting(from: now, to: now, to: .main, withHandler: { (_, error: Error?) in
-                if let error = error as NSError?,
-                   error.domain == CMErrorDomain,
-                   error.code == CMErrorMotionActivityNotAuthorized.rawValue {
-                    completionHandler(false, .denied)
-                    return
-                }
-                completionHandler(true, .authorized)
-            })
+            handleMotionPermissionRequest(completionHandler)
         case .focus:
-            Current.focusStatus.requestAuthorization().done { status in
-                completionHandler(status == .authorized, status.genericStatus)
+            handleFocusPermissionRequest(completionHandler)
+        }
+    }
+
+    private func handlePermissionsThatWereDeniedPreviously(_ completionHandler: @escaping (Bool, PermissionStatus) -> Void) {
+        let destination: UIApplication.OpenSettingsDestination
+
+        switch self {
+        case .location: destination = .location
+        case .motion: destination = .motion
+        case .focus: destination = .focus
+        }
+
+        UIApplication.shared.openSettings(destination: destination, completionHandler: nil)
+        completionHandler(false, .denied)
+    }
+
+    private func handleFocusPermissionRequest(_ completionHandler: @escaping (Bool, PermissionStatus) -> Void) {
+        Current.focusStatus.requestAuthorization().done { status in
+            completionHandler(status == .authorized, status.genericStatus)
+        }
+    }
+
+    private func handleLocationPermissionRequest(_ completionHandler: @escaping (Bool, PermissionStatus) -> Void) {
+        if PermissionsLocationDelegate.shared == nil {
+            PermissionsLocationDelegate.shared = PermissionsLocationDelegate()
+        }
+
+        PermissionsLocationDelegate.shared!.requestPermission { status in
+            DispatchQueue.main.async {
+                completionHandler(status == .authorized || status == .authorizedWhenInUse, status)
+                PermissionsLocationDelegate.shared = nil
             }
         }
+    }
+
+    private func handleMotionPermissionRequest(_ completionHandler: @escaping (Bool, PermissionStatus) -> Void) {
+        let manager = CMMotionActivityManager()
+        let now = Date()
+
+        manager.queryActivityStarting(from: now, to: now, to: .main, withHandler: { (_, error: Error?) in
+            if let error = error as NSError?,
+               error.domain == CMErrorDomain,
+               error.code == CMErrorMotionActivityNotAuthorized.rawValue {
+                completionHandler(false, .denied)
+                return
+            }
+            completionHandler(true, .authorized)
+        })
     }
 }
