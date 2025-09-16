100faf37f8d7298343fa17509d31bd9e8063af48
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2025-09-16 11:10:13 -0300

    WIP

---
+++ Sources/App/Onboarding/Steps/Permissions/BasePermissionView.swift
@@ -0,0 +1,19 @@
+//
+//  BasePermissionView.swift
+//  App
+//
+//  Created by Bruno Pantaleão on 16/9/25.
+//  Copyright © 2025 Home Assistant. All rights reserved.
+//
+
+import SwiftUI
+
+struct BasePermissionView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BasePermissionView()
+}
