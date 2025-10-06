76512e29da04116822ff695e02ed0c08bb18e150
Ikrma Iftikhar <ikrmaiftikhar3@gmail.com>
2025-10-06 23:31:26 +0500

    Name and Images changed

--- Sources/Shared/Settings/AppSessionValues.swift
+++ Sources/Shared/Settings/AppSessionValues.swift
@@ -3,7 +3,6 @@ import Foundation
 public protocol AppSessionValuesProtocol {
     var inviteURL: URL? { get set }
 }
-
 /// Values stored for the given app session until terminated by the OS.
 final class AppSessionValues: AppSessionValuesProtocol {
     static var shared = AppSessionValues()
