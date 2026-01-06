521771a4d85fca63c0f2fb66d333229335adb6f9
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-01-06 03:59:34 +0100

    Add area and device names to entity display
    Enhanced Cover, Fan, Switch, and AppIntent entities to include areaName and deviceName properties. Updated displayRepresentation to show these as subtitles, improving context in UI. Refactored entity queries to fetch and assign area and device names, and streamlined filtering logic.

--- Sources/Extensions/Widgets/Controls/Fan/IntentFanEntity.swift
+++ Sources/Extensions/Widgets/Controls/Fan/IntentFanEntity.swift
@@ -14,22 +14,50 @@ struct IntentFanEntity: AppEntity {
     var id: String
     var entityId: String
     var serverId: String
+    var areaName: String?
+    var deviceName: String?
     var displayString: String
     var iconName: String
     var displayRepresentation: DisplayRepresentation {
-        DisplayRepresentation(title: "\(displayString)")
+        DisplayRepresentation(
+            title: "\(displayString)",
+            subtitle: .init(stringLiteral: subtitle)
+        )
+    }
+
+    private var subtitle: String {
+        var subtitle = ""
+        if let areaName {
+            subtitle += areaName
+        }
+
+        if let deviceName,
+           deviceName.trimmingCharacters(in: .whitespacesAndNewlines).lowercased() != displayString.lowercased()
+           .trimmingCharacters(in: .whitespacesAndNewlines) {
+            if subtitle.isEmpty {
+                subtitle += deviceName
+            } else {
+                subtitle += " → \(deviceName)"
+            }
+        }
+
+        return subtitle
     }
 
     init(
         id: String,
         entityId: String,
         serverId: String,
+        areaName: String? = nil,
+        deviceName: String? = nil,
         displayString: String,
         iconName: String
     ) {
         self.id = id
         self.entityId = entityId
         self.serverId = serverId
+        self.areaName = areaName
+        self.deviceName = deviceName
         self.displayString = displayString
         self.iconName = iconName
     }
@@ -42,12 +70,12 @@ struct IntentFanAppEntityQuery: EntityQuery, EntityStringQuery {
     }
 
     func entities(matching string: String) async throws -> IntentItemCollection<IntentFanEntity> {
-        let fansPerServer = await getFanEntities()
+        let fansPerServer = await getFanEntities(matching: string)
 
         return .init(sections: fansPerServer.map { (key: Server, value: [IntentFanEntity]) in
             .init(
                 .init(stringLiteral: key.info.name),
-                items: value.filter({ $0.displayString.lowercased().contains(string.lowercased()) })
+                items: value
             )
         })
     }
@@ -65,11 +93,15 @@ struct IntentFanAppEntityQuery: EntityQuery, EntityStringQuery {
         let entities = ControlEntityProvider(domains: [.fan]).getEntities(matching: string)
 
         for (server, values) in entities {
+            let deviceMap = values.devicesMap(for: server.identifier.rawValue)
+            let areasMap = values.areasMap(for: server.identifier.rawValue)
             fanEntities.append((server, values.map({ entity in
                 IntentFanEntity(
                     id: entity.id,
                     entityId: entity.entityId,
                     serverId: entity.serverId,
+                    areaName: areasMap[entity.entityId]?.name ?? "",
+                    deviceName: deviceMap[entity.entityId]?.name ?? "",
                     displayString: entity.name,
                     iconName: entity.icon ?? SFSymbol.fan.rawValue
                 )
