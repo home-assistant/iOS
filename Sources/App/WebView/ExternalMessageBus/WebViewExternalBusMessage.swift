a9bd2e0821c40b3122d3bf0e6bcf8004f8617168
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-01-08 17:00:14 +0100

    Do not allow toast on mac catalyst

--- Sources/App/WebView/ExternalMessageBus/WebViewExternalBusMessage.swift
+++ Sources/App/WebView/ExternalMessageBus/WebViewExternalBusMessage.swift
@@ -40,7 +40,7 @@ enum WebViewExternalBusMessage: String, CaseIterable {
             "downloadFileSupported": true,
             "appVersion": "\(AppConstants.version) (\(AppConstants.build))",
             "toastComponentVersion": { // Frontend can use this to know if the version has what it needs
-                if #available(iOS 18, *) {
+                if #available(iOS 18, *), !Current.isCatalyst {
                     return ToastManager.toastComponentVersion
                 } else {
                     return -1
