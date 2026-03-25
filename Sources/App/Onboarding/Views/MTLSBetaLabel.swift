f0d0cf183d5873a34f2db1e704ce1bd4134810c5
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-03-25 14:33:08 +0100

    Update MTLSBetaLabel.swift

--- Sources/App/Onboarding/Views/MTLSBetaLabel.swift
+++ Sources/App/Onboarding/Views/MTLSBetaLabel.swift
@@ -2,7 +2,6 @@ import Shared
 import SwiftUI
 
 struct MTLSBetaLabel: View {
-    @State private var showExplanation = false
     var body: some View {
         BetaLabel(info: L10n.Mtls.Beta.info)
     }
