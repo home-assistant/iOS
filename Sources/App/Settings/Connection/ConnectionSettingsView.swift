e5130275e1e2bde2c4c3fb1a42c903003bc365aa
Bruno Pantaleão Gonçalves <5808343+bgoncal@users.noreply.github.com>
2026-03-31 10:58:25 +0200

    Improve labs label spacing and rename it (#4462)
    <!-- Thank you for submitting a Pull Request and helping to improve Home
    Assistant. Please complete the following sections to help the processing
    and review of your changes. Please do not delete anything from this
    template. -->
    
    ## Summary
    <!-- Provide a brief summary of the changes you have made and most
    importantly what they aim to achieve -->
    
    ## Screenshots
    <!-- If this is a user-facing change not in the frontend, please include
    screenshots in light and dark mode. -->
    <img width="2030" height="494" alt="CleanShot 2026-03-31 at 10 36 48@2x"
    src="https://github.com/user-attachments/assets/539853f4-f8dc-46d6-a167-25af8ca0b399"
    />
    
    ## Link to pull request in Documentation repository
    <!-- Pull requests that add, change or remove functionality must have a
    corresponding pull request in the Companion App Documentation repository
    (https://github.com/home-assistant/companion.home-assistant). Please add
    the number of this pull request after the "#" -->
    Documentation: home-assistant/companion.home-assistant#
    
    ## Any other notes
    <!-- If there is any other information of note, like if this Pull
    Request is part of a bigger change, please include it here. -->

--- Sources/App/Settings/Connection/ConnectionSettingsView.swift
+++ Sources/App/Settings/Connection/ConnectionSettingsView.swift
@@ -409,7 +409,7 @@ struct ConnectionSettingsView: View {
         } header: {
             HStack {
                 Text(L10n.Settings.ConnectionSection.ClientCertificate.header)
-                MTLSBetaLabel()
+                MTLSLabsLabel()
             }
         } footer: {
             Text(L10n.Settings.ConnectionSection.ClientCertificate.footer)
