787e88fd1f472468be2132cb569102df7c403de3
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-04-14 16:12:30 +0200

    Rename matter device after commissioning

--- Sources/Extensions/Matter/HATypedRequest+Matter.swift
+++ Sources/Extensions/Matter/HATypedRequest+Matter.swift
@@ -1,6 +1,16 @@
 import HAKit
 import Shared
 
+struct MatterConfigEntry: Codable, HADataDecodable {
+    let entryId: String
+    let domain: String
+
+    init(data: HAData) throws {
+        self.entryId = try data.decode("entry_id")
+        self.domain = try data.decode("domain")
+    }
+}
+
 extension HATypedRequest {
     static func matterCommission(
         code: String
@@ -8,6 +18,25 @@ extension HATypedRequest {
         HATypedRequest<HAResponseVoid>(request: .init(
             type: "matter/commission",
             data: ["code": code]
+        ))
+    }
+
+    static func configEntriesList() -> HATypedRequest<[MatterConfigEntry]> {
+        HATypedRequest<[MatterConfigEntry]>(request: .init(
+            type: "config_entries/get"
+        ))
+    }
+
+    static func updateDeviceRegistry(
+        deviceId: String,
+        nameByUser: String
+    ) -> HATypedRequest<DeviceRegistryEntry> {
+        HATypedRequest<DeviceRegistryEntry>(request: .init(
+            type: "config/device_registry/update",
+            data: [
+                "device_id": deviceId,
+                "name_by_user": nameByUser,
+            ]
         ))
     }
 }
