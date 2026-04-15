3864f30e16143ef24070e608c1809b80ea0fdd51
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-04-15 17:26:58 +0200

    Add Labs badge to assist inside CarPlay config

--- Sources/Shared/MagicItem/MagicItem+Defaults.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-import UIKit
-
-public extension MagicItem {
-    static var defaultAssistIconColorHex: String {
-        Color.haPrimary.hex() ?? UIColor(Color.haPrimary).hexString()
-    }
-}
