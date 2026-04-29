33e464eb0d33065b8e73754d3cfa8b4fcd928ff1
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-04-29 16:44:02 +0200

    Remove legacy actions

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -418,7 +418,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private func setupModels() {
         // Force Realm migration to happen now
         _ = Realm.live()
-        Action.setupObserver()
         NotificationCategory.setupObserver()
     }
 
