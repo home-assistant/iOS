5d433cf8e888b5f21f4dd9a6296cb5b12e89ee6e
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-05-06 16:52:46 +0200

    Update testing.rb

--- fastlane/lanes/testing.rb
+++ fastlane/lanes/testing.rb
@@ -40,6 +40,6 @@ lane :test do
     result_bundle: true,
     skip_package_dependencies_resolution: true,
     skip_detect_devices: true,
-    destination: 'platform=iOS Simulator,name=iPhone 17,OS=26.4'
+    destination: 'platform=iOS Simulator,name=iPhone 17,OS=latest'
   )
 end
