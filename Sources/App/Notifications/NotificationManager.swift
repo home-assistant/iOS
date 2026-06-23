896962d34a8ff4a67d60b341e15bd308ecede96f
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-06-23 23:53:00 +0200

    Update NotificationManager.swift

--- Sources/App/Notifications/NotificationManager.swift
+++ Sources/App/Notifications/NotificationManager.swift
@@ -478,8 +478,8 @@ extension NotificationManager: UNUserNotificationCenterDelegate {
         }
 
         guard let command = KioskPushCommand(message: message) else {
-            Current.Log.warning("Received unhandled kiosk push command: \(message)")
-            return []
+            Current.Log.warning("Unhandled kiosk push command, using default presentation: \(message)")
+            return nil
         }
 
         performKioskCommand(command, userInfo: content.userInfo)
