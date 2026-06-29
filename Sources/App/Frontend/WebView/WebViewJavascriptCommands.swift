f59b4a32c389d9d4cf80134898e88637d795e2c1
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-06-29 13:35:18 +0200

    Improve and fix issue where gestures were not sending correct JS command to frontend

--- Sources/App/Frontend/WebView/WebViewJavascriptCommands.swift
+++ Sources/App/Frontend/WebView/WebViewJavascriptCommands.swift
@@ -1,64 +1,24 @@
 import Foundation
 
 enum WebViewJavascriptCommands {
-    static var searchEntitiesKeyEvent = """
-    var event = new KeyboardEvent('keydown', {
-        key: 'e',
-        code: 'KeyE',
-        keyCode: 69,
-        which: 69,
-        bubbles: true,
-        cancelable: true
-    });
-    document.dispatchEvent(event);
-    """
+    static var searchEntitiesKeyEvent = keyDownEvent(key: "e", code: "KeyE", keyCode: 69)
+    static var quickSearchKeyEvent = keyDownEvent(key: "k", code: "KeyK", keyCode: 75, metaKey: true)
+    static var searchDevicesKeyEvent = keyDownEvent(key: "d", code: "KeyD", keyCode: 68)
+    static var searchCommandsKeyEvent = keyDownEvent(key: "c", code: "KeyC", keyCode: 67)
+    static var assistKeyEvent = keyDownEvent(key: "a", code: "KeyA", keyCode: 65)
 
-    static var quickSearchKeyEvent = """
-    var event = new KeyboardEvent('keydown', {
-        key: 'k',
-        code: 'KeyK',
-        keyCode: 75,
-        which: 75,
-        metaKey: true,
-        bubbles: true,
-        cancelable: true
-    });
-    document.dispatchEvent(event);
-    """
-
-    static var searchDevicesKeyEvent = """
-    var event = new KeyboardEvent('keydown', {
-        key: 'd',
-        code: 'KeyD',
-        keyCode: 68,
-        which: 68,
-        bubbles: true,
-        cancelable: true
-    });
-    document.dispatchEvent(event);
-    """
-
-    static var searchCommandsKeyEvent = """
-    var event = new KeyboardEvent('keydown', {
-        key: 'c',
-        code: 'KeyC',
-        keyCode: 67,
-        which: 67,
-        bubbles: true,
-        cancelable: true
-    });
-    document.dispatchEvent(event);
-    """
-
-    static var assistKeyEvent = """
-    var event = new KeyboardEvent('keydown', {
-        key: 'a',
-        code: 'KeyA',
-        keyCode: 65,
-        which: 65,
-        bubbles: true,
-        cancelable: true
-    });
-    document.dispatchEvent(event);
-    """
+    private static func keyDownEvent(key: String, code: String, keyCode: Int, metaKey: Bool = false) -> String {
+        """
+        var event = new KeyboardEvent('keydown', {
+            key: '\(key)',
+            code: '\(code)',
+            keyCode: \(keyCode),
+            which: \(keyCode),
+            metaKey: \(metaKey),
+            bubbles: true,
+            cancelable: true
+        });
+        (document.body || document.documentElement || document).dispatchEvent(event);
+        """
+    }
 }
