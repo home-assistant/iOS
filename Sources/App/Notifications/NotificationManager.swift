60201c89e462e6280d90d998a279bba0348d42ef
Bruno Pantaleão Gonçalves <5808343+bgoncal@users.noreply.github.com>
2026-07-07 10:34:27 +0200

    Add manager callback url scheme check when setup notifications (#4959)
    <!-- Thank you for submitting a Pull Request and helping to improve Home
    Assistant. Please complete the following sections to help the processing
    and review of your changes. Please do not delete anything from this
    template. -->
    
    ## Summary
    <!-- Provide a brief summary of the changes you have made and most
    importantly what they aim to achieve -->
    
    ## Screenshots
    <!-- If this is a user-facing change not in the frontend, please include
    screenshots in light and dark mode. -->
    
    ## Link to pull request in Documentation repository
    <!-- Pull requests that add, change or remove functionality must have a
    corresponding pull request in the Companion App Documentation repository
    (https://github.com/home-assistant/companion.home-assistant). Please add
    the number of this pull request after the "#" -->
    Documentation: home-assistant/companion.home-assistant#
    
    ## Any other notes
    <!-- If there is any other information of note, like if this Pull
    Request is part of a bigger change, please include it here. -->

--- Sources/App/Notifications/NotificationManager.swift
+++ Sources/App/Notifications/NotificationManager.swift
@@ -52,6 +52,9 @@ class NotificationManager: NSObject, LocalPushManagerDelegate {
     func setupNotifications() {
         UNUserNotificationCenter.current().delegate = self
         _ = localPushManager
+        if Manager.shared.callbackURLScheme == nil {
+            Manager.shared.callbackURLScheme = Manager.urlSchemes?.first
+        }
     }
 
     @objc private func didBecomeActive() {
